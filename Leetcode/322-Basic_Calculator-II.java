63cdeea70598afe678fe441e82129b8e6fcd51c4
Mitshah2406 <mitshah2406@gmail.com>
2024-07-23 23:05:31 +0530

    Infix eval

---
+++ Leetcode/322-Basic_Calculator-II.java
@@ -0,0 +1,71 @@
+class Solution {
+    private boolean isOperator(char ch) {
+        return ch == '+' || ch == '-' || ch == '*' || ch == '/';
+    }
+
+    private int getPrecedence(char ch) {
+        if (ch == '*' || ch == '/') {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private int calculate(int a, int b, char operation) {
+        if (operation == '+') {
+            return a + b;
+        } else if (operation == '-') {
+            return a - b;
+        } else if (operation == '*') {
+            return a * b;
+        } else {
+            return a / b;
+        }
+    }
+
+    public int calculate(String s) {
+        Stack<Integer> operand = new Stack();
+        Stack<Character> operator = new Stack();
+
+        int i = 0;
+        int n = s.length();
+
+        while (i < n) {
+            char ch = s.charAt(i);
+
+            if (ch >= '0' && ch <= '9') {
+                int num = 0;
+                while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+                    num = num * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                // System.out.println(num);
+                operand.push(num);
+                i--;
+            } else if (isOperator(ch)) {
+                while (operator.size() > 0 && getPrecedence(operator.peek()) >= getPrecedence(ch)) {
+                    char oper = operator.pop();
+                    int val2 = operand.pop();
+                    int val1 = operand.pop();
+
+                    int ans = calculate(val1, val2, oper);
+                    // System.out.println(ans);
+                    operand.push(ans);
+                }
+                operator.push(ch);
+            }
+            i++;
+        }
+
+        while (operator.size() > 0) {
+            char oper = operator.pop();
+            int val2 = operand.pop();
+            int val1 = operand.pop();
+
+            int ans = calculate(val1, val2, oper);
+            //   System.out.println(ans);
+            operand.push(ans);
+        }
+        return operand.pop();
+    }
+}
