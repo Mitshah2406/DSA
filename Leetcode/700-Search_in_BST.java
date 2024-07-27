a772042fca2db74e8d54158ae0c73737d5ad477a
Mitshah2406 <mitshah2406@gmail.com>
2024-07-28 00:06:49 +0530

    fix

--- Leetcode/700-Search_in_BST.java
+++ Leetcode/700-Search_in_BST.java
@@ -1,22 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
 class Solution {
-    public boolean canPlaceFlowers(int[] arr, int n) {
-        int len = arr.length+2;
-        int dup[] = new int[len];
-        for(int i=0;i<len-2;i++){
-            dup[i+1]=arr[i];
-          
-        }
-        for(int i=1;i<len-1;i++){
-              if(n==0){
-                break;
+    public TreeNode searchBST(TreeNode root, int val) {
+        // if(root==null || root.val==val){
+        // return root;
+        // }else if(root.val<val){
+        // root = searchBST(root.right, val);
+        // }else{
+        // root = searchBST(root.left, val);
+        // }
+        // return root;
+
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else if (root.val < val) {
+                root = root.right;
+            } else {
+                root = root.left;
             }
-            if(dup[i]==0){
-                if(dup[i-1] ==0 && dup[i+1]==0){
-                    n--;
-                    dup[i]=1;
-                }
-            }
         }
-        return n == 0;
+        return root;
     }
 }
