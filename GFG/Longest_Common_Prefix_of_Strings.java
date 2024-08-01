d9d6088b95534b23d49d6e09fc3c07c5bc8fa873
Mit Shah <mitshah2406@gmail.com>
2024-08-01 17:43:20 +0530

    Added Longest_Common_Prefix_of_Strings.java

--- GFG/Longest_Common_Prefix_of_Strings.java
+++ GFG/Longest_Common_Prefix_of_Strings.java
@@ -19,36 +19,12 @@ class GFG {
 // } Driver Code Ends
 
 
-
-
 // User function Template for Java
 
 class Solution {
     public String longestCommonPrefix(String arr[]) {
-        String least = arr[0];
-        int n = arr.length;
-        for(int i=1;i<n;i++){
-            if(least.length()>arr[i].length()){
-                least=arr[i];
-            }
-        }
-        // System.out.println("Least"+least);
-        // char toMatch[] = least.toCharArray();
-        for(String s: arr){
-            // System.out.println(s);
-            for(int j=0;j<least.length() && j<s.length();j++){
-                if(least.charAt(j)!=s.charAt(j)){
-                // System.out.println("In"+j);
-                    if(j==0){
-                        least="";
-                    }else{
-                        // System.out.println(toMatch.length+" ->"+j);
-                        least = least.substring(0,j);
-                    }
-                        break;
-                }
-            }
-        }
-        return (least.equals("") == true ? "-1" : least);
+        // code here
+        
+        Hello
     }
 }
