e1833bc5ccdd62cf9947886f741baf0a0e782e0a
Mit Shah <mitshah2406@gmail.com>
2024-08-04 00:28:32 +0530

    Added 1460-Make_Two_Arrays_Equal_by_Reversing_Subarrays.java

--- Leetcode/1460-Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
+++ Leetcode/1460-Make_Two_Arrays_Equal_by_Reversing_Subarrays.java
@@ -1,14 +1,50 @@
 class Solution {
-    public boolean canBeEqual(int[] targetArray, int[] currentArray) {
-       
-        int[] elementCounts = new int[1001];
-        int uniqueCount = 0;
-        
-        for (int i = 0; i < targetArray.length; i++) {
-            if (elementCounts[targetArray[i]]++ == 0) uniqueCount++;
-            if (elementCounts[currentArray[i]]-- == 1) uniqueCount--;
+    public boolean canBeEqual(int[] target, int[] arr) {
+
+        // 1st - nlogn -
+        // Arrays.sort(arr);
+        // Arrays.sort(target);
+
+        // for (int i = 0; i < target.length; i++) {
+        // if (target[i] != arr[i])
+        // return false;
+        // }
+        // return true;
+
+        // O(2N)
+        // HashMap<Integer, Integer> hm = new HashMap();
+
+        // for (int i : arr) {
+        // hm.put(i, hm.getOrDefault(i, 0) + 1);
+        // }
+
+        // for (int i : target) {
+        // if (!hm.containsKey(i)) {
+        // return false;
+        // } else {
+        // int val = hm.get(i);
+        // val--;
+        // hm.put(i, val);
+        // if (val <= 0) {
+        // hm.remove(i);
+        // }
+        // }
+        // }
+        // return true;
+
+        // Optimal Soln
+        int ans[] = new int[1001];
+        for (int i : target) {
+            ans[i]++;
         }
-        
-        return uniqueCount == 0;
+
+        for (int i : arr) {
+            ans[i]--;
+        }
+        for (int i : ans) {
+            if (i != 0)
+                return false;
+        }
+        return true;
     }
 }
