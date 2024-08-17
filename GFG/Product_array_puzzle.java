96aff14bbb244882d168ee8716257caf89f3dedd
Mit Shah <mitshah2406@gmail.com>
2024-08-17 11:31:52 +0530

    Added Product_array_puzzle.java

---
+++ GFG/Product_array_puzzle.java
@@ -0,0 +1,28 @@
+// User function Template for Java
+
+class Solution {
+    
+    // brute Force
+    // public static long[] productExceptSelf(int nums[]) {
+    //     long ans[] = new long[nums.length];
+    //     Arrays.fill(ans,1);
+    //     int n = nums.length;
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<n;j++){
+    //             if(i==j) continue;
+    //             ans[i] *= nums[j];
+    //         }
+    //     }
+        
+    //     return ans;
+    // }
+    
+     public static long[] productExceptSelf(int nums[]) {
+        long ans[] = new long[nums.length];
+        Arrays.fill(ans,1);
+        int n = nums.length;
+        
+        
+        return ans;
+    }
+}
