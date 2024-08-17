52aca0ad1414056edcf1c379fb4cbd8609575bc1
Mit Shah <mitshah2406@gmail.com>
2024-08-17 15:35:52 +0530

    Added 554-Brick_Wall.java

--- Leetcode/554-Brick_Wall.java
+++ Leetcode/554-Brick_Wall.java
@@ -2,18 +2,16 @@ class Solution {
     public int leastBricks(List<List<Integer>> wall) {
         HashMap<Integer, Integer> hm = new HashMap();
 
+        int max = 0;
         for(List<Integer> w: wall){
             int p = 0;
             for(int i=0;i<w.size()-1;i++){
                 p+=w.get(i);
                 hm.put(p, hm.getOrDefault(p, 0)+1);
+                max = Math.max(max, hm.get(p));
             }
         }
-        int max = 0;
-        for(Map.Entry<Integer,Integer> e : hm.entrySet()){
-
-            max = Math.max(max, e.getValue());
-        }
+       
 
         return wall.size()-max;
     }
