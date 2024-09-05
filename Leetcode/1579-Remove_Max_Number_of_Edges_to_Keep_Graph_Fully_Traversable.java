da8b41168ce7fb1928a6975b958555ceef02a433
Mit Shah <mitshah2406@gmail.com>
2024-09-05 13:15:25 +0530

    Added 1579-Remove_Max_Number_of_Edges_to_Keep_Graph_Fully_Traversable.java

--- Leetcode/1579-Remove_Max_Number_of_Edges_to_Keep_Graph_Fully_Traversable.java
+++ Leetcode/1579-Remove_Max_Number_of_Edges_to_Keep_Graph_Fully_Traversable.java
@@ -1,78 +1,94 @@
-class Solution {
-    public int maxNumEdgesToRemove(int n, int[][] edges) {
-        UnionFind alice = new UnionFind(n);
-        UnionFind bob = new UnionFind(n);
+class DisjointSet {
+    public int parent[];
+    public int rank[];
 
-        int edgesRequired = 0;
-        // First process type 3 edges
-        for (int[] edge : edges) {
-            if (edge[0] == 3) {
-                edgesRequired += (alice.preformUnion(edge[1], edge[2]) | bob.preformUnion(edge[1], edge[2]));
-            }
+    public DisjointSet(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
         }
-
-        // Process type 2 and type 1 edges
-        for (int[] edge : edges) {
-            if (edge[0] == 2) {
-                edgesRequired += bob.preformUnion(edge[1], edge[2]);
-            } else if (edge[0] == 1) {
-                edgesRequired += alice.preformUnion(edge[1], edge[2]);
-            }
-        }
-
-        // Check if both Alice's and Bob's graphs are fully connected
-        if (alice.isConnected() && bob.isConnected()) {
-            return edges.length - edgesRequired;
-        }
-
-        return -1;
     }
 
-    class UnionFind {
-        int[] representative;
-        int[] componentSize;
-        int components;
-
-        UnionFind(int n) {
-            components = n;
-            representative = new int[n + 1];
-            componentSize = new int[n + 1];
-
-            for (int i = 0; i <= n; i++) {
-                representative[i] = i;
-                componentSize[i] = 1;
-            }
+    public int find(int x) {
+        if (parent[x] == x) {
+            return x;
         }
 
-        int findRepresentative(int x) {
-            if (representative[x] == x) {
-                return x;
-            }
-            return representative[x] = findRepresentative(representative[x]);
+        int ans = find(parent[x]);
+        parent[x] = ans;
+        return ans;
+    }
+
+    public boolean unionByRank(int x, int y) {
+        int parX = find(x);
+        int parY = find(y);
+        if (parX == parY) {
+            return false;
         }
-
-        int preformUnion(int x, int y) {
-            x = findRepresentative(x);
-            y = findRepresentative(y);
-
-            if (x == y) {
-                return 0;
-            }
-
-            if (componentSize[x] > componentSize[y]) {
-                componentSize[x] += componentSize[y];
-                representative[y] = x;
-            } else {
-                componentSize[y] += componentSize[x];
-                representative[x] = y;
-            }
-
-            components--;
-            return 1;
+        if (rank[parX] > rank[parY]) {
+            parent[parY] = parX;
+        } else if (rank[parX] < rank[parY]) {
+            parent[parX] = parY;
+        } else {
+            parent[parY] = parX;
+            rank[parX]++;
         }
+        return true;
+    }
+}
 
-        boolean isConnected() {
-            return components == 1;
+class Solution {
+    public int maxNumEdgesToRemove(int n, int[][] edges) {
+        DisjointSet alice = new DisjointSet(n+1);
+        DisjointSet bob = new DisjointSet(n+1);
+        // A --> Alice, B --> Bob
+        Arrays.sort(edges, (a,b) -> b[0]-a[0]);
+        int edgeRemoveCount = 0;
+        int nodesConsideredA = 1;
+        int nodesConsideredB = 1;
+        for(int edge[]: edges){
+            int type = edge[0];
+            int u = edge[1];
+            int v = edge[2];
+
+            if(type==3){
+                boolean mergeA = alice.unionByRank(u,v); // for alice
+                boolean mergeB = bob.unionByRank(u,v); // for bob
+
+                if(!mergeA && !mergeB){
+                    edgeRemoveCount++;
+                }
+                if(mergeA){
+                    nodesConsideredA++;
+                }
+                if(mergeB){
+                    nodesConsideredB++;
+                }
+            }else if(type==2){
+                // Bob
+                boolean mergeB = bob.unionByRank(u,v); // for bob
+
+                if(!mergeB){
+                   edgeRemoveCount++;
+                }else{
+                    nodesConsideredB++;
+                }
+            }else{
+                // Alice
+                boolean mergeA = alice.unionByRank(u,v); // for alice
+
+                if(!mergeA){
+                    edgeRemoveCount++;
+                }else{
+                    nodesConsideredA++;
+                }
+            }
         }
+        if(nodesConsideredA!=n || nodesConsideredB!=n){
+            return -1;
+        }
+        return edgeRemoveCount;
     }
 }
