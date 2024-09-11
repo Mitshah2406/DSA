19c348323a43ac8e20dd9e0b1a4c1599638797c6
Mit Shah <mitshah2406@gmail.com>
2024-09-11 23:54:51 +0530

    Added 2220-Minimum_Bit_Flips_to_Convert_Number.java

--- Leetcode/2220-Minimum_Bit_Flips_to_Convert_Number.java
+++ Leetcode/2220-Minimum_Bit_Flips_to_Convert_Number.java
@@ -8,14 +8,10 @@ class Solution {
     }
     public int minBitFlips(int start, int goal) {
         if(start==goal) return 0;
-        // StringBuilder og = new StringBuilder();
-        // StringBuilder target = new StringBuilder();
-
-
         int ans=0;
         int place = 0;
         for(int i=0;i<32;i++){
-            place = place + (int)Math.pow(2,i);
+            place = place + (1<<i);
             char f = '0';
             char s = '0';
             if(checkBit(start,i)){
