7f0ce1737c497ce01d0f9738e93b860c6930ea30
Mit Shah <mitshah2406@gmail.com>
2024-09-11 23:58:22 +0530

    Added 2220-Minimum_Bit_Flips_to_Convert_Number.java

--- Leetcode/2220-Minimum_Bit_Flips_to_Convert_Number.java
+++ Leetcode/2220-Minimum_Bit_Flips_to_Convert_Number.java
@@ -3,7 +3,6 @@ class Solution {
         if((n&(1<<k))==0){
             return false;
         }
-
         return true;
     }
     public int minBitFlips(int start, int goal) {
@@ -11,16 +10,9 @@ class Solution {
         int ans=0;
         int place = 0;
         for(int i=0;i<32;i++){
-            place = place + (1<<i);
-            char f = '0';
-            char s = '0';
-            if(checkBit(start,i)){
-                f = '1';
-            }
-            if(checkBit(goal,i)){
-                s = '1';
-            }
-            if(f!=s){
+            place = place + (1<<i); // binary number logic ==> ...64 32 16 8 4 2 1
+            if((checkBit(start,i) ^ checkBit(goal,i))){ 
+                // if bits are diff then xor will result in true, which we want
                 ans++;
             }
             if(start<place && goal<place){
