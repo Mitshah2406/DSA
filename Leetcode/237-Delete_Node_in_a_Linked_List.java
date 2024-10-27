15582c10eaf950e766306bd44cdb82eb9574d396
Mit Shah <mitshah2406@gmail.com>
2024-10-27 17:33:42 +0530

    Added 237-Delete_Node_in_a_Linked_List.java

--- Leetcode/237-Delete_Node_in_a_Linked_List.java
+++ Leetcode/237-Delete_Node_in_a_Linked_List.java
@@ -8,7 +8,7 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-
+        // Brute force shift all later values by 1 step forward and then remove last node
         // we cant remvoe this specific node but can remove next by copying next ki
         // value into curr(node)
         ListNode nextNode = node.next;
