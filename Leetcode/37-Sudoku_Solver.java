1a72109d9eb9c1a8ccafc48e91aa8bc0afe882de
Mit Shah <mitshah2406@gmail.com>
2024-10-27 14:39:46 +0530

    Added 37-Sudoku_Solver.java

--- Leetcode/37-Sudoku_Solver.java
+++ Leetcode/37-Sudoku_Solver.java
@@ -1,67 +1,51 @@
 class Solution {
-    public void solveSudoku(char[][] board) {
-        // Start solving sudoku from the first cell
-        solve(board, 0, 0);
-    }
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
 
-    private boolean solve(char[][] board, int row, int col) {
-        // Base case: If row is equal to board length, entire board has been filled
-        if (row == board.length) {
-            return true;
-        }
-        
-        // Move to next row when current row is fully filled
-        if (col == board[0].length) {
-            return solve(board, row + 1, 0);
-        }
-        
-        // Skip cells that are already filled
-        if (board[row][col] != '.') {
-            return solve(board, row, col + 1);
-        }
-        
-        // Try different numbers in current cell
-        for (char num = '1'; num <= '9'; num++) {
-            if (isValidPlacement(board, row, col, num)) {
-                board[row][col] = num; // Fill current cell with valid number
-                
-                // Move to next cell
-                if (solve(board, row, col + 1)) {
-                    return true;
+                if (board[i][j] == '.') {
+                    for (char x = '1'; x <= '9'; x++) {
+                        if (valid(board, i, j, x)) {
+                            board[i][j] = x;
+                            boolean ans = solve(board);
+                            if (ans==true) {return true;}
+                            else {board[i][j] = '.';}
+                        }
+                    }
+                    return false;
                 }
-                
-                // Backtrack to previous state if solution not found
-                board[row][col] = '.';
             }
         }
-        
-        // No valid solution found
-        return false;
+
+        return true;
     }
 
-    private boolean isValidPlacement(char[][] board, int row, int col, char num) {
-        // Check if num is already in the same row, column or 3x3 subgrid
-        for (int i = 0; i < board.length; i++) {
-            // Check row
-            if (board[i][col] == num) {
-                return false;
-            }
+    public boolean valid(char[][] board, int row, int col, char x) {
+        // find in row & col
 
-            // Check column
-            if (board[row][i] == num) {
-                return false;
-            }
- 
-            // Check 3x3 subgrid
-            int subgridRow = 3 * (row / 3) + i / 3; // Calculate row index of subgrid
-            int subgridCol = 3 * (col / 3) + i % 3; // Calculate column index of subgrid
- 
-            if (board[subgridRow][subgridCol] == num) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == x || board[row][i] == x) {
                 return false;
             }
         }
 
-        // Placement is valid
+        // find minibox(subbox) top left coords
+        int topLeftRowCorner = 3 * (row / 3);
+        int topLeftColCorner = 3 * (col / 3);
+
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 2; j++) {
+                if (board[topLeftRowCorner + i][topLeftColCorner + j] == x) {
+                    return false;
+                }
+            }
+        }
+
+        // all cases pass
         return true;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
     }
 }
