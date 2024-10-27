9d405abff94565355f7f54afa0d7ecf94608399e
Mit Shah <mitshah2406@gmail.com>
2024-10-27 17:33:03 +0530

    Added 237-Delete_Node_in_a_Linked_List.java

---
+++ Leetcode/237-Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+
+        // we cant remvoe this specific node but can remove next by copying next ki
+        // value into curr(node)
+        ListNode nextNode = node.next;
+
+        node.val = nextNode.val;
+
+        node.next = nextNode.next;
+
+    }
+}
