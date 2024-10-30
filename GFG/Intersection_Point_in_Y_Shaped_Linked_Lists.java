a2dc7a2b7f162036782d970e776085ebaed5e291
Mit Shah <mitshah2406@gmail.com>
2024-10-30 19:20:39 +0530

    Added Intersection_Point_in_Y_Shaped_Linked_Lists.java

---
+++ GFG/Intersection_Point_in_Y_Shaped_Linked_Lists.java
@@ -0,0 +1,42 @@
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+ Linked List class
+class LinkedList
+{
+    Node head;  // head of list
+}*/
+
+class Intersect {
+    // Function to find intersection point in Y shaped Linked Lists.
+    int intersectPoint(Node head1, Node head2) {
+        
+        // Brute with extra space
+        // HashSet<Node> hs = new HashSet();
+        
+        // Node temp1 = head1;
+        
+        // while(temp1!=null){
+        //     hs.add(temp1);
+        //     temp1=temp1.next;
+        // }
+        
+        
+        //  temp1 = head2;
+        
+        // while(temp1!=null){
+        //     if(hs.contains(temp1)){
+        //         return temp1.data;
+        //     }
+        //     temp1=temp1.next;
+        // }
+        
+        // return -1;
+        
+        
+    }
+    
+}
