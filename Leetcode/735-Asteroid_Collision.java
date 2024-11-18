34bc70326292e832e369d247ee5cbe0607bbaf02
Mit Shah <mitshah2406@gmail.com>
2024-11-18 17:05:24 +0530

    Added 735-Asteroid_Collision.java

--- Leetcode/735-Asteroid_Collision.java
+++ Leetcode/735-Asteroid_Collision.java
@@ -1,40 +1,43 @@
 class Solution {
-    public int[] asteroidCollision(int[] a) {
+    public int[] asteroidCollision(int[] asteroids) {
         Stack<Integer> st = new Stack();
-
-        int n = a.length;
+        int n = asteroids.length;
         for (int i = 0; i < n; i++) {
-            int as = a[i];
-            boolean isFirstDestroyed = false;
-            while (st.size() > 0 && (as < 0 && st.peek() > 0)) { // colliding
-                int first = Math.abs(as);
-                int scd = Math.abs(st.peek());
-
-                if (first == scd) {
-                    isFirstDestroyed = true;
-                    st.pop();
-                    break;
-                } else if (first < scd) {
-                    isFirstDestroyed = true;
-                    break;
-                } else {
-                    st.pop();
+            int curr = asteroids[i];
+            if (st.size() > 0 && (st.peek() > 0 && curr < 0)) {
+                // collide
+                boolean isCurrPushed = false;
+                while (st.size() > 0 && (st.peek() > 0 && curr < 0)) {
+                    int first = Math.abs(st.peek());
+                    int scd = Math.abs(curr);
+                    if (first < scd) {
+                        st.pop();
+                        isCurrPushed = true;
+                    } else if (first > scd) {
+                        isCurrPushed = false;
+                        break;
+                    } else {
+                        st.pop();
+                        isCurrPushed = false;
+                        break;
+                    }
                 }
+                if (isCurrPushed)
+                    st.push(curr);
+            } else {
+                st.push(curr);
             }
-            if (!isFirstDestroyed) {
-                st.push(as);
-            }
-
-            
-
+            // System.out.println(st);
         }
         int n1 = st.size();
         int ans[] = new int[n1];
         int it = n1 - 1;
+
         while (!st.isEmpty()) {
             ans[it] = st.pop();
             it--;
         }
+
         return ans;
     }
 }
