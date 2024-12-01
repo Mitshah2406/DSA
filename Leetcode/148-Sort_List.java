2c4183591f05374ff7e370316c6629f10dc4414c
Mit Shah <mitshah2406@gmail.com>
2024-12-01 13:37:18 +0530

    Added 148-Sort_List.java

--- Leetcode/148-Sort_List.java
+++ Leetcode/148-Sort_List.java
@@ -1,75 +1,49 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
-class Solution {
-    private ListNode merge(ListNode l1, ListNode l2) {
-        ListNode p1 = l1;
-        ListNode p2 = l2;
-        ListNode p3 = new ListNode(-1);
-        ListNode temp = p3;
-        while (p1 != null && p2 != null) {
-            if (p1.val >= p2.val) {
-                temp.next = new ListNode(p2.val);
-                p2 = p2.next;
-            } else {
-                temp.next = new ListNode(p1.val);
-                p1 = p1.next;
-            }
-            temp = temp.next;
-        }
-
-        if (p1 == null) {
-            while (p2 != null) {
-                temp.next = new ListNode(p2.val);
-                p2 = p2.next;
-                temp = temp.next;
-            }
-        }
-        if (p2 == null) {
-            while (p1 != null) {
-                temp.next = new ListNode(p1.val);
-                p1 = p1.next;
-                temp = temp.next;
-            }
-        }
-
-        return p3.next;
+public class Solution {
+  
+  public ListNode sortList(ListNode head) {
+    if (head == null || head.next == null)
+      return head;
+        
+    // step 1. cut the list to two halves
+    ListNode prev = null, slow = head, fast = head;
+    
+    while (fast != null && fast.next != null) {
+      prev = slow;
+      slow = slow.next;
+      fast = fast.next.next;
     }
-
-
-
-    private ListNode findMid(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-
-        while (fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        return slow;
-
+    
+    prev.next = null;
+    
+    // step 2. sort each half
+    ListNode l1 = sortList(head);
+    ListNode l2 = sortList(slow);
+    
+    // step 3. merge l1 and l2
+    return merge(l1, l2);
+  }
+  
+  ListNode merge(ListNode l1, ListNode l2) {
+    ListNode l = new ListNode(0), p = l;
+    
+    while (l1 != null && l2 != null) {
+      if (l1.val < l2.val) {
+        p.next = l1;
+        l1 = l1.next;
+      } else {
+        p.next = l2;
+        l2 = l2.next;
+      }
+      p = p.next;
     }
+    
+    if (l1 != null)
+      p.next = l1;
+    
+    if (l2 != null)
+      p.next = l2;
+    
+    return l.next;
+  }
 
-    public ListNode sortList(ListNode h1) {
-        if (h1==null || h1.next==null){
-            return h1;
-        }
-        ListNode mid = findMid(h1);
-        ListNode one = h1;
-        ListNode two = mid.next;
-        mid.next = null;
-        one = sortList(one);
-        two = sortList(two);
-        ListNode t3 = merge(one,two);
-        return t3;
-    }
 }
