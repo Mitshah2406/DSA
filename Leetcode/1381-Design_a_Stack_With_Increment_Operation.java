fabd1a38f7770a050a39a11627e170e0f252ed9e
Mit Shah <mitshah2406@gmail.com>
2024-12-04 13:04:31 +0530

    Added 1381-Design_a_Stack_With_Increment_Operation.java

--- Leetcode/1381-Design_a_Stack_With_Increment_Operation.java
+++ Leetcode/1381-Design_a_Stack_With_Increment_Operation.java
@@ -1,23 +1,58 @@
 class CustomStack {
-    ArrayList<Integer> st;
+
+    // Better Approach
+    // ArrayList<Integer> st;
+    // int currSize;
+    // int k;
+    // public CustomStack(int maxSize) {
+    //     k = maxSize;
+    //     currSize=0;
+    //     st = new ArrayList();
+    // }
+    
+    // public void push(int x) {
+    //     if(currSize<k){
+    //         st.add(x);
+    //         currSize++;
+    //     }
+    // }
+    
+    // public int pop() {
+    //     if(currSize!=0){
+    //         return st.remove(--currSize);
+    //     }else{
+    //         return -1;
+    //     }
+    // }
+    
+    // public void increment(int k, int val) {
+    //     for(int i=0;i<Math.min(k,currSize);i++){
+    //         int old = st.get(i);
+    //         st.set(i, old+val);
+    //     }
+    // }
+
+    // Best
+    int[] arr;
     int currSize;
     int k;
     public CustomStack(int maxSize) {
         k = maxSize;
         currSize=0;
-        st = new ArrayList();
+        arr = new int[maxSize];
+        Arrays.fill(arr,-1);
     }
     
     public void push(int x) {
         if(currSize<k){
-            st.add(x);
+            arr[currSize] = x;
             currSize++;
         }
     }
     
     public int pop() {
         if(currSize!=0){
-            return st.remove(--currSize);
+            return arr[--currSize];
         }else{
             return -1;
         }
@@ -25,8 +60,7 @@ class CustomStack {
     
     public void increment(int k, int val) {
         for(int i=0;i<Math.min(k,currSize);i++){
-            int old = st.get(i);
-            st.set(i, old+val);
+            arr[i] = arr[i] +val;
         }
     }
 }
