de5b1162fb4e765daa351054af12a96e385e54c9
Mit Shah <mitshah2406@gmail.com>
2025-01-02 19:30:14 +0530

    Added Left_View_of_Binary_Tree.java

---
+++ GFG/Left_View_of_Binary_Tree.java
@@ -0,0 +1,40 @@
+// User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class Solution {
+    // Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> leftView(Node root) {
+        ArrayList<Integer> res = new ArrayList();
+        if(root==null){
+            return res;
+        }
+        Queue<Node> q = new LinkedList();
+        
+        q.add(root);
+        
+        while(q.size()!=0){
+            int n = q.size();
+            
+            for(int i=0;i<n;i++){
+                Node rem = q.poll();
+                if(i==0){
+                    res.add(rem.data);
+                }
+                if(rem.left!=null) q.add(rem.left);
+                if(rem.right!=null) q.add(rem.right);
+            }
+        }
+        return res;
+    }
+}
