e1ab45da0d3a560df4a2fae209b2b23acac215a4
Mit Shah <mitshah2406@gmail.com>
2025-01-02 16:40:04 +0530

    Added 94-Binary_Tree_Inorder_Traversal.java

--- Leetcode/94-Binary_Tree_Inorder_Traversal.java
+++ Leetcode/94-Binary_Tree_Inorder_Traversal.java
@@ -1,48 +1,30 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
+    private void inorder(List<Integer> arr, TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder(arr, root.left);
+        arr.add(root.val);
+        inorder(arr, root.right);
+    }
     public List<Integer> inorderTraversal(TreeNode root) {
-            TreeNode curr = root;
-            List<Integer> arr = new ArrayList();
-            while(curr!=null){
-                if(curr.left==null){
-                    // print root and go right
-                    arr.add(curr.val);
-                    curr = curr.right;
-                }else{
-                    // take left in new pointer
-                    TreeNode currp1 = curr.left;
-                    // go towards the end in left branch of curr i.e right branch of currp1
-                    while(currp1.right!=null && currp1.right!=curr){
-                        currp1 = currp1.right;
-                    }
-                    // if currp1.right is null make a conenction to curr and then proceed curr to left
-                    if(currp1.right==null){
-                        currp1.right = curr;
-                        curr = curr.left;
-                    }
-                    else if(currp1.right==curr){
-                        // else print curr and remove conenction proceed to right of curr
-                        arr.add(curr.val);
-                        currp1.right = null;
-                        curr = curr.right;
-                    }
-                }
-            }
-
-            return arr;
+        List<Integer> arr = new ArrayList();
+        inorder(arr, root);
+        return arr;
     }
 }
