8fe4e28a13f41b37faa4bd7c71687a545ea3db16
Mit Shah <mitshah2406@gmail.com>
2025-01-13 23:07:40 +0530

    Added 1038-Binary_Search_Tree_to_Greater_Sum_Tree.java

--- Leetcode/1038-Binary_Search_Tree_to_Greater_Sum_Tree.java
+++ Leetcode/1038-Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -1,43 +1,61 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
-    static int sum = 0;
-    public void inorder(TreeNode root){
-        if(root==null){
-            return;
-        }
-        inorder(root.left);
-        sum+=root.val;
-        inorder(root.right);
-    }
-    public void inorder1(TreeNode root){
-        if(root==null){
-            return;
-        }
-        inorder1(root.left);
-        sum-=root.val;
+    // brute force
+    // static int sum = 0;
+    // public void inorder(TreeNode root){
+    // if(root==null){
+    // return;
+    // }
+    // inorder(root.left);
+    // sum+=root.val;
+    // inorder(root.right);
+    // }
+    // public void inorder1(TreeNode root){
+    // if(root==null){
+    // return;
+    // }
+    // inorder1(root.left);
+    // sum-=root.val;
+    // root.val+=sum;
+    // inorder1(root.right);
+    // }
+    // public TreeNode bstToGst(TreeNode root) {
+    // sum = 0;
+    // inorder(root);
+    // inorder1(root);
+    // return root;
 
-        root.val+=sum;
-        inorder1(root.right);
+    // }
+
+    // optimal - go right to left do dry run
+    static int sum = 0;
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.right);
+        sum += root.val;
+        root.val = sum;
+        inorder(root.left);
     }
+
     public TreeNode bstToGst(TreeNode root) {
         sum = 0;
         inorder(root);
-        inorder1(root);
         return root;
-        
     }
 }
