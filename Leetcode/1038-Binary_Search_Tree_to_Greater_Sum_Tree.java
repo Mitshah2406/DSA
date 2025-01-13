c6b8e396a77525c9f25b35f510a76fe17657a200
Mit Shah <mitshah2406@gmail.com>
2025-01-13 23:03:29 +0530

    Added 1038-Binary_Search_Tree_to_Greater_Sum_Tree.java

--- Leetcode/1038-Binary_Search_Tree_to_Greater_Sum_Tree.java
+++ Leetcode/1038-Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -1,34 +1,43 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- * int val;
- * TreeNode left;
- * TreeNode right;
- * TreeNode() {}
- * TreeNode(int val) { this.val = val; }
- * TreeNode(int val, TreeNode left, TreeNode right) {
- * this.val = val;
- * this.left = left;
- * this.right = right;
- * }
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
  * }
  */
 class Solution {
     static int sum = 0;
-
-    public static void inorder(TreeNode root) {
-        if (root == null)
+    public void inorder(TreeNode root){
+        if(root==null){
             return;
-        inorder(root.right);
-        sum += root.val;
-        root.val = sum;
+        }
         inorder(root.left);
+        sum+=root.val;
+        inorder(root.right);
     }
+    public void inorder1(TreeNode root){
+        if(root==null){
+            return;
+        }
+        inorder1(root.left);
+        sum-=root.val;
 
+        root.val+=sum;
+        inorder1(root.right);
+    }
     public TreeNode bstToGst(TreeNode root) {
-
         sum = 0;
         inorder(root);
+        inorder1(root);
         return root;
+        
     }
 }
