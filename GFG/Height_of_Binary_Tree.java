a6f10e61c7e437300039c7a6e2278456899796b9
Mit Shah <mitshah2406@gmail.com>
2025-01-16 18:31:08 +0530

    Added Height_of_Binary_Tree.java

--- GFG/Height_of_Binary_Tree.java
+++ GFG/Height_of_Binary_Tree.java
@@ -16,20 +16,34 @@ class Node
 
 class Solution {
     // Function to find the height of a binary tree.
-    static int ans = 0;
-    public void preorder(Node root, int ht){
+    // static int ans = 0;
+    // public void preorder(Node root, int ht){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     ans = Math.max(ans, ht);
+    //     preorder(root.left,ht+1);
+    //     preorder(root.right,ht+1);
+        
+        
+    // }
+    
+
+    public int preorder(Node root){
         if(root==null){
-            return;
+            return 0;
         }
-        ans = Math.max(ans, ht);
-        preorder(root.left,ht+1);
-        preorder(root.right,ht+1);
+
+        int x = preorder(root.left);
+        int y = preorder(root.right);
         
-        
+        return Math.max(x,y)+1;
     }
     int height(Node node) {
-        ans = 0;
-        preorder(node, 0);
-        return ans;
+        // ans = 0;
+        // preorder(node, 0);
+        // return ans;
+        // minus 1 coz we are counting nodes so edges are nodes-1
+        return preorder(node)-1;
     }
 }
