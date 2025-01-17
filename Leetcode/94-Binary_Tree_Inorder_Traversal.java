bceaa7c3dac7166705c82c4a9c97166281b41713
Mit Shah <mitshah2406@gmail.com>
2025-01-17 20:47:24 +0530

    Added 94-Binary_Tree_Inorder_Traversal.java

--- Leetcode/94-Binary_Tree_Inorder_Traversal.java
+++ Leetcode/94-Binary_Tree_Inorder_Traversal.java
@@ -24,7 +24,36 @@ class Solution {
     }
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> arr = new ArrayList();
-        inorder(arr, root);
+        // inorder(arr, root);
+        // morris (inorder - left,node,right)
+
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left==null){
+                // if left is null process node and go right
+                arr.add(curr.val);
+                curr = curr.right;
+            }else{
+                // if left is non null
+                TreeNode currp1 = curr.left; 
+
+                while(currp1.right!=null && currp1.right!=curr){
+                    currp1 = currp1.right;
+                }
+
+                if(currp1.right==null){
+                    // if first time then establish link and then move curr to left
+                    currp1.right = curr;
+                    curr = curr.left;
+                }else{
+                    // if second time print node, destroy link, move to right
+
+                    currp1.right = null;
+                    arr.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
         return arr;
     }
 }
