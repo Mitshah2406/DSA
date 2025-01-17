c3f2cb04691686a7f3ed5b1fbbb3fd9b15748a1a
Mit Shah <mitshah2406@gmail.com>
2025-01-17 19:18:51 +0530

    Added 449-Serialize_and_Deserialize_BST.java

--- Leetcode/449-Serialize_and_Deserialize_BST.java
+++ Leetcode/449-Serialize_and_Deserialize_BST.java
@@ -2,46 +2,74 @@
  * Definition for a binary tree node.
  * public class TreeNode {
  *     int val;
- *     TreeNode left;
+ *     TreeNode right;
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
  */
 public class Codec {
-    private void serializeHelper(TreeNode root, StringBuilder s){
+
+    // Encodes a tree to a single string.
+    public void preorder(TreeNode root, StringBuilder sb){
         if(root==null){
-            s.append("N ");
             return;
         }
-        s.append(root.val +" ");
-        serializeHelper(root.left, s);
-        serializeHelper(root.right, s);
+        sb.append(root.val+"_");
+        preorder(root.left, sb);
+        preorder(root.right, sb);
     }
-    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        StringBuilder s = new StringBuilder();
-        serializeHelper(root, s);
-        return s.toString();
-    }
-    int i;
-    // Decodes your encoded data to tree.
-    public TreeNode deserialize(String data) {
-        String ch[] = data.split(" ");
-        i = 0;
-        TreeNode root = deserializeHelper(ch);
-        return root;
+        StringBuilder sb = new StringBuilder();
+        preorder(root,sb);
+        return sb.toString();
     }
 
-    private TreeNode deserializeHelper(String[] ch){
-        if(ch[i].equals("N")){
-            i++;
+    // Decodes your encoded data to tree.
+    static int i=0;
+    // public TreeNode build(String arr[]){
+    //     if(i>=arr.length){
+    //         return null;
+    //     }
+    //     if(arr[i].equals("N")){
+    //         i++;
+    //         return null;
+    //     }
+    //     TreeNode root = new TreeNode(Integer.parseInt(arr[i++]));
+    //     root.left = build(arr);
+    //     root.right = build(arr);
+
+    //     return root;
+    // }
+
+    public TreeNode build(String arr[], int st, int end){
+
+        if(st>end){ // when st cross end return null
             return null;
         }
-        TreeNode root = new TreeNode(Integer.parseInt(ch[i]));
-        i++;
-        root.left = deserializeHelper(ch);
-        root.right = deserializeHelper(ch);
+        int idx = st;
+
+        // exploit bst property just find right part in arr 
+        for(idx=st;idx<=end;idx++){
+            if(Integer.parseInt(arr[idx])>Integer.parseInt(arr[st])){
+                break;
+            }
+        }
+        // st is root as it is preorder
+        TreeNode root = new TreeNode(Integer.parseInt(arr[st++]));
+        // values before right part idx are on left
+        root.left = build(arr,st,idx-1);
+        // values from idx to end are right
+        root.right = build(arr,idx,end);
         return root;
+    }
+    public TreeNode deserialize(String data) {
+        if(data.length()==0){
+            return null;
+        }
+        i=0;
+        String arr[] = data.split("_");
+        int n = arr.length;
+        return build(arr, 0, n-1);
     }
 }
 
