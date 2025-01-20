d7cdc8f8b3abc760d6ead0fddff74892233252b3
Mit Shah <mitshah2406@gmail.com>
2025-01-20 20:32:39 +0530

    Added 547-Number_of_Provinces.java

--- Leetcode/547-Number_of_Provinces.java
+++ Leetcode/547-Number_of_Provinces.java
@@ -1,59 +1,25 @@
-class DisjointSet {
-    public int parent[];
-    public int rank[];
-
-    public DisjointSet(int n) {
-        parent = new int[n];
-        rank = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
-    }
-
-    public void unionByRank(int x, int y) {
-        int parX = find(x);
-        int parY = find(y);
-
-        if (parX == parY)
-            return;
-
-        if (rank[parX] > rank[parY]) {
-            parent[parY] = parX;
-        } else if (rank[parY] > rank[parX]) {
-            parent[parX] = parY;
-        } else {
-            parent[parY] = parX;
-            rank[parX]++;
-        }
-    }
-
-    public int find(int x) {
-        if (parent[x] == x) {
-            return x;
-        }
-        int temp = find(parent[x]);
-        parent[x] = temp;
-        return temp;
-    }
-
-}
-
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        DisjointSet ds = new DisjointSet(n);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(isConnected[i][j]==1){
-                    ds.unionByRank(i,j);
-                }
+    public void dfs(int arr[][], boolean vis[], int i, int n) {
+        vis[i] = true;
+
+        for (int k=0;k<n;k++) {
+            if(!vis[k] && arr[i][k]==1){
+                dfs(arr,vis,k,n);
             }
         }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            if(i==ds.parent[i]){
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        // Not the most optimal but multi source DFS
+        // can also do with adj list construction
+
+        int n = isConnected.length;
+        boolean vis[] = new boolean[n];
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(isConnected, vis, i, n);
                 ans++;
             }
         }
