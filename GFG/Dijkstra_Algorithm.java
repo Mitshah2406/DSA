265a2692020d923130f88b759a37c40f60265479
Mit Shah <mitshah2406@gmail.com>
2025-01-21 17:34:10 +0530

    Added Dijkstra_Algorithm.java

--- GFG/Dijkstra_Algorithm.java
+++ GFG/Dijkstra_Algorithm.java
@@ -1,56 +1,45 @@
-//User function Template for Java
+/*
+class iPair {
+    int first, second;
 
-class Pair implements Comparable<Pair>{
-    int v;
-    int wt;
-    public Pair(int v, int w){
-        this.v= v;
-        this.wt = w;
-    }
-    public int compareTo(Pair n){
-        return this.wt - n.wt;
+    iPair(int first, int second) {
+        this.first = first;
+        this.second = second;
     }
 }
-class Solution
-{
-    
-    static int[] dijkstra(int V, ArrayList<ArrayList<ArrayList<Integer>>> adj, int S)
-    {
-       ArrayList<ArrayList<Pair>> graph = new ArrayList<>();
-       
-       for(int i=0;i<V;i++){
-           graph.add(new ArrayList());
-       }
-       int i = 0;
-       for(ArrayList<ArrayList<Integer>> edges : adj){
-           for(ArrayList<Integer> edge: edges){
-               graph.get(i).add(new Pair(edge.get(0), edge.get(1)));
-           }
-           i++;
-       }
-       
-       
-       PriorityQueue<Pair> pq = new PriorityQueue<Pair>();
-       pq.add(new Pair(S, 0));
-       int dist[] = new int[V];
-       Arrays.fill(dist, Integer.MAX_VALUE);
-       
-       while(pq.size()>0){
-           Pair rem = pq.remove();
-           
-           ArrayList<Pair> nbrs = graph.get(rem.v);
-           
-           if(dist[rem.v]!=Integer.MAX_VALUE) continue;
-           
-           dist[rem.v] = rem.wt;
-           for(Pair p:nbrs){
-               if(dist[p.v]!=Integer.MAX_VALUE) continue;
-               
-               pq.add(new Pair(p.v, rem.wt+p.wt));
-           }
-       }
-       
-       return dist;
+*/
+
+// User function Template for Java
+class Solution {
+    // Function to find the shortest distance of all the vertices
+    // from the source vertex src.
+    ArrayList<Integer> dijkstra(ArrayList<ArrayList<iPair>> adj, int src) {
+    PriorityQueue<iPair> q = new PriorityQueue<>((a, b) -> Integer.compare(a.second, b.second));
+      q.add(new iPair(src,0));
+        int n = adj.size();
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
+        while(q.size()!=0){
+            iPair rem = q.poll();
+            if(rem.second<dist[rem.first]){
+                dist[rem.first] = rem.second;
+            }
+            
+            for(iPair ip : adj.get(rem.first)){
+                int nVtx = ip.first;
+                int cost = ip.second;
+                
+                if(cost+rem.second<dist[nVtx]){
+                    q.add(new iPair(nVtx, cost+rem.second));
+                }
+            }
+        }
+        ArrayList<Integer> arr = new ArrayList();
+        for(int i=0;i<n;i++){
+            arr.add(dist[i]);
+        }
+        return arr;
+        
     }
 }
-
