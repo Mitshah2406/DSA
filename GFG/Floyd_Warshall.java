75bd0bb7749b0a8302b02a8eb0636965c8f64144
Mit Shah <mitshah2406@gmail.com>
2025-01-22 13:37:32 +0530

    Added Floyd_Warshall.java

---
+++ GFG/Floyd_Warshall.java
@@ -0,0 +1,41 @@
+// User function template for JAVA
+
+class Solution {
+    public void shortestDistance(int[][] mat) {
+        int n = mat.length;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] ==-1){
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(i==j){
+                        continue;
+                    }
+                    
+                    if(mat[i][k]!=Integer.MAX_VALUE
+                    && mat[k][j]!=Integer.MAX_VALUE
+                    && mat[i][k]+mat[k][j]<mat[i][j]
+                    ){
+                       mat[i][j] =  mat[i][k]+mat[k][j];
+                    }
+                }
+            }
+        }
+        
+ for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j] ==Integer.MAX_VALUE){
+                    mat[i][j] = -1;
+                }
+               
+            }
+            
+        }
+    }
+}
