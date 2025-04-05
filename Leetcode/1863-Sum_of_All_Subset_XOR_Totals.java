99e2db4551420a98303e954c1c14b6c8e84f430d
Mit Shah <mitshah2406@gmail.com>
2025-04-05 11:28:28 +0530

    Added 1863-Sum_of_All_Subset_XOR_Totals.java

--- Leetcode/1863-Sum_of_All_Subset_XOR_Totals.java
+++ Leetcode/1863-Sum_of_All_Subset_XOR_Totals.java
@@ -6,19 +6,30 @@ class Solution {
         return true;
     }
 
+    // public int subsetXORSum(int[] nums) {
+    //     int n = nums.length;
+    //     int sum = 0;
+    //     int total = 1 << n;
+    //     for (int i = 1; i < total; i++) {
+    //         int xor = 0;
+    //         for (int j = 0; j < n; j++) {
+    //             if (checkBit(i, j)) {
+    //                 xor = xor^nums[j];
+    //             }
+    //         }
+    //         sum+=xor;
+    //     }
+    //     return sum;
+    // }
     public int subsetXORSum(int[] nums) {
-        int n = nums.length;
-        int sum = 0;
-        int total = 1 << n;
-        for (int i = 1; i < total; i++) {
-            int xor = 0;
-            for (int j = 0; j < n; j++) {
-                if (checkBit(i, j)) {
-                    xor = xor^nums[j];
-                }
-            }
-            sum+=xor;
-        }
-        return sum;
+    int result = 0;
+    
+    // Bitwise OR all numbers to get bits that appear in any number
+    for (int num : nums) {
+        result |= num;
     }
+    System.out.println(result);
+    // Each bit contributes 2^(n-1) to the final sum
+    return result * (1 << (nums.length-1));
+}
 }
