48b0d6bb36fe2c10b73a98917504103cf65b4ac7
Mit Shah <mitshah2406@gmail.com>
2025-04-08 21:42:10 +0530

    Added 62-Unique_Paths.java

--- Leetcode/62-Unique_Paths.java
+++ Leetcode/62-Unique_Paths.java
@@ -1,17 +1,29 @@
 class Solution {
-    public int uniquePaths(int m, int n) {
-        int dp[][] = new int[m][n];
-
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0||j==0){
-                    dp[i][j] = 1;
-                }else{
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
-                }
-            }
+    /*
+    TC = O(m*n)
+    SC = O(m*n)
+    */
+    public int find(int row, int col, int i, int j, int dp[][]) {
+        if (i == row - 1 && j == col - 1) {
+            return 1;
         }
+        if (i > row - 1 || j > col - 1) {
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int bottom = find(row, col, i+1, j, dp);
+        int right = find(row, col, i, j+1, dp);
 
-        return dp[m-1][n-1];
+        return dp[i][j] = bottom + right;
+    }
+
+    public int uniquePaths(int m, int n) {
+        int dp[][]= new int[m][n];
+        for(int d[]: dp){
+            Arrays.fill(d,-1);
+        }
+        return find(m, n, 0, 0,dp);
     }
 }
