f9a257605b864d16300e51d7b8d181be527cb5bb
Mit Shah <mitshah2406@gmail.com>
2025-06-21 16:40:52 +0530

    Added 1-Two_Sum.java

--- Leetcode/1-Two_Sum.java
+++ Leetcode/1-Two_Sum.java
@@ -1,35 +1,18 @@
 class Solution {
-    public int[] twoSum(int[] arr, int target) {
-
-        int n = arr.length;
-        int ans[] = new int[2];
-        // brute - O(n^2)
-        // for(int i=0;i<n;i++){
-        //     for(int j=i+1;j<n;j++){
-        //         if(arr[i]+arr[j]==target){
-        //             ans[0] = i;
-        //             ans[1] = j;
-        //             return ans;
-        //         }
-        //     }
-        // }
-        // return ans;
-
-        // better - O(n) with space
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
 
         HashMap<Integer, Integer> hm = new HashMap();
-
+        int ans[] = new int[2];
         for(int i=0;i<n;i++){
-            if(hm.containsKey(target-arr[i])){
-                ans[0]=i;
-                ans[1] = hm.get(target-arr[i]);
+            if(hm.containsKey(target-nums[i])){
+                ans[0] = i;
+                ans[1] = hm.get(target-nums[i]);
                 break;
+            }else{
+                hm.put(nums[i], i);
             }
-            hm.put(arr[i], i);
         }
-
         return ans;
-
-       
     }
 }
