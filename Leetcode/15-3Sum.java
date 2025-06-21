8a4382547f9b77e04c528354c25458720d1446f8
Mit Shah <mitshah2406@gmail.com>
2025-06-21 16:01:31 +0530

    Added 15-3Sum.java

--- Leetcode/15-3Sum.java
+++ Leetcode/15-3Sum.java
@@ -1,102 +1,89 @@
-// Medium
-
-// Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
-
-// Notice that the solution set must not contain duplicate triplets.
-
- 
-
-// Example 1:
-
-// Input: nums = [-1,0,1,2,-1,-4]
-// Output: [[-1,-1,2],[-1,0,1]]
-// Explanation: 
-// nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
-// nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
-// nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
-// The distinct triplets are [-1,0,1] and [-1,-1,2].
-// Notice that the order of the output and the order of the triplets does not matter.
-// Example 2:
-
-// Input: nums = [0,1,1]
-// Output: []
-// Explanation: The only possible triplet does not sum up to 0.
-// Example 3:
-
-// Input: nums = [0,0,0]
-// Output: [[0,0,0]]
-// Explanation: The only possible triplet sums up to 0.
- 
-
-// Constraints:
-
-// 3 <= nums.length <= 3000
-// -105 <= nums[i] <= 105
-
-
-//Link --> https://leetcode.com/problems/3sum/description/
-
-//Most Optimal (two pointer approach)
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
+        // brute
+        int n = nums.length;
+        HashSet<List<Integer>> hs = new HashSet();
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i + 1; j < n; j++) {
+        //         for (int k = j + 1; k < n; k++) {
+        //             if (nums[i] + nums[j] + nums[k] == 0) {
+        //                 List<Integer> arr = new ArrayList();
+        //                 arr.add(nums[i]);
+        //                 arr.add(nums[j]);
+        //                 arr.add(nums[k]);
+        //                 Collections.sort(arr);
+        //                 hs.add(arr);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // List<List<Integer>> ans = new ArrayList();
+        // for (List<Integer> l : hs) {
+        //     ans.add(l);
+        // }
+
+        // return ans;
+
+        // better
+        // HashMap<Integer, Integer> hm = new HashMap();
+        // Arrays.sort(nums);
+        // for (int i = 0; i < n; i++) {
+        //     HashSet<Integer> t = new HashSet();
+        //     for (int j = i + 1; j < n; j++) {
+        //         int sum = (nums[i] + nums[j]);
+        //         if (t.contains(-sum)) {
+                    // List<Integer> arr = new ArrayList();
+                    // arr.add(nums[i]);
+                    // arr.add(nums[j]);
+                    // arr.add(-sum);
+                    // hs.add(arr);
+        //         }
+        //         t.add(nums[j]);
+        //     }
+        // }
+
+        // List<List<Integer>> ans = new ArrayList();
+        // for (List<Integer> l : hs) {
+        //     ans.add(l);
+        // }
+
+        // return ans;
+
+
+        // best - two pointer approach
+
+        // i as constant till j crosses k
+        // if sum < 0 increment j
+        // if sum > 0 decrement k
+        // otherwise add the pair, and discard the same elements
 
         Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int j = i + 1;
-            int k = n - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum > 0) {
+        List<List<Integer>> ans = new ArrayList();
+        for(int i=0;i<n;i++){
+            int j = i+1;
+            int k = n-1;
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            while(j<k){
+                int sum = nums[j]+nums[k]+nums[i];
+                if(sum>0){
                     k--;
-                } else if (sum < 0) {
+                }else if(sum<0){
                     j++;
-                } else {
-                    List<Integer> ans = Arrays.asList(nums[i], nums[j], nums[k]);
-                    res.add(ans);
-                    j++;
+                }else{
+                    List<Integer> arr = new ArrayList();
+                    arr.add(nums[i]);
+                    arr.add(nums[j]);
+                    arr.add(nums[k]);
+                    ans.add(arr);
                     k--;
-                    while (j < k && nums[j] == nums[j - 1])
-                        j++;
-                    while (j < k && nums[k] == nums[k + 1])
-                        k--;
+                    j++;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
                 }
             }
         }
-        return res;
+
+        return ans;
     }
 }
-// TC - O(n^2)
-// SC - O(1)
-// using hashset
-
-class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        HashSet<List<Integer>> ans = new HashSet<>();
-        Arrays.sort(nums);
-        int n = nums.length;
-        for (int i = 0; i < n; i++) {
-            HashSet<Integer> hs = new HashSet();
-            for (int j = i + 1; j < n; j++) {
-                int k = -(nums[i] + nums[j]);
-                if (hs.contains(k) == true) {
-                    List<Integer> a = Arrays.asList(nums[i], nums[j], k);
-                    ans.add(a);
-                }
-                hs.add(nums[j]);
-            }
-        }
-
-       res.addAll(ans);
-
-        return res;
-    }
-}
-
-// TC - O(n^2)
-// SC - O(n)
