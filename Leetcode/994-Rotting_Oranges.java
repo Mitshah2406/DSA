3cc98638d94f8d08867973c9089b55f833f4d62b
Mit Shah <mitshah2406@gmail.com>
2025-06-27 18:40:31 +0530

    Added 994-Rotting_Oranges.java

--- Leetcode/994-Rotting_Oranges.java
+++ Leetcode/994-Rotting_Oranges.java
@@ -6,62 +6,56 @@ class Solution {
      * Time - O(n*m){Initial Src Finding} + O(m*n){BFS} = O(2(m*n)) ~ O(m*n)
      * Space - O(m*n){Queue Space} + O(m*n) {Visited Arr space} = O(2(m*n)) ~ O(m*n)
      */
-    class Pair {
+    class Pair{
         int i;
         int j;
         int t;
-
-        public Pair(int i, int j, int t) {
-            this.i = i;
-            this.j = j;
-            this.t = t;
+        public Pair(int i, int j, int t){
+            this.i=i;
+            this.j=j;
+            this.t=t;
         }
     }
-
-    public boolean isInBounds(int i, int j, int n, int m) {
-        return (i >= 0 && j >= 0 && i < n && j < m);
+    public boolean isInBounds(int i, int j, int m, int n){
+        return (i>=0 && j>=0 && j<m && i<n);
     }
-
     public int orangesRotting(int[][] grid) {
-        Queue<Pair> q = new LinkedList();
-        int n = grid.length;
         int m = grid[0].length;
-        boolean vis[][] = new boolean[n][m];
-        int dirs[][] = {
-                { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 }
-        };
-        int fresh = 0;
+        int n = grid.length;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 2) {
-                    q.add(new Pair(i, j, 0));
-                } else if (grid[i][j] == 1) {
-                    fresh++;
+        Queue<Pair> q = new LinkedList();
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.add(new Pair(i,j,0));
+                }else if(grid[i][j]==1){
+                    count++;
                 }
             }
         }
-        int time = 0;
-        while (q.size() != 0) {
+        int dirs[][] = {
+            {0,1}, {1,0},{-1,0},{0,-1}
+        };
+        int max = 0;
+        while(!q.isEmpty()){
             Pair rem = q.poll();
-            int r = rem.i;
-            int c = rem.j;
+            int u = rem.i;
+            int v = rem.j;
             int t = rem.t;
-            time = t;
-            for (int i = 0; i < 4; i++) {
-                int nR = r + dirs[i][0];
-                int nC = c + dirs[i][1];
-                if (isInBounds(nR, nC, n, m) && grid[nR][nC] == 1 && !vis[nR][nC]) {
-                    vis[nR][nC] = true;
-                    grid[nR][nC] = 2;
-                    fresh--;
-                    q.add(new Pair(nR, nC, t + 1));
+            max = Math.max(t,max);
+
+            for(int i=0;i<4;i++){
+                int nr = u + dirs[i][0];
+                int nc = v + dirs[i][1];
+
+                if(isInBounds(nr,nc,m,n) && grid[nr][nc]==1){
+                    count--;
+                    q.add(new Pair(nr,nc, t+1));
+                    grid[nr][nc]=2;
                 }
             }
         }
-        if (fresh != 0) {
-            return -1;
-        }
-        return time;
+        return count==0 ? max : -1;
     }
 }
