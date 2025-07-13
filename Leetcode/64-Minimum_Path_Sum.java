dfa158b4560b342833964b43a6265a9fb3f24b72
Mit Shah <mitshah2406@gmail.com>
2025-07-13 20:44:44 +0530

    Added 64-Minimum_Path_Sum.java

--- Leetcode/64-Minimum_Path_Sum.java
+++ Leetcode/64-Minimum_Path_Sum.java
@@ -1,32 +1,76 @@
 class Solution {
-    /*
-    TC => O(2) {cost of one fn} X No of recursive Calls {m*n} = O(m*n)
-    SC => O(m*n) {Space}
-    */
-    public int calc(int grid[][], int n, int m, int i, int j, int dp[][]) {
-        if (i >= n || j >= m)
-            return Integer.MAX_VALUE;
-
-        if (i == n - 1 && j == m - 1)
+    public int minPath(int i, int j, int grid[][], int dp[][]) {
+        if (i == 0 && j == 0) {
             return grid[i][j];
-
+        }
+        if (i < 0 || j < 0) {
+            return (int) 1e9;
+        }
         if (dp[i][j] != -1)
             return dp[i][j];
+        int left = minPath(i, j - 1, grid, dp) + grid[i][j];
+        int up = minPath(i - 1, j, grid, dp) + grid[i][j];
 
-        int down = calc(grid, n, m, i + 1, j, dp);
-        int right = calc(grid, n, m, i, j + 1, dp);
-
-        return dp[i][j] = grid[i][j] + Math.min(down, right);
+        return dp[i][j] = Math.min(left, up);
     }
 
     public int minPathSum(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
+        // int rows = grid.length;
+        // int cols = grid[0].length;
+        // int dp[][] = new int[rows][cols];
+        // for (int d[] : dp)
+        //     Arrays.fill(d, -1);
+        // return minPath(rows - 1, cols - 1, grid, dp);
 
-        int dp[][] = new int[n + 1][m + 1];
-        for (int d[] : dp) {
-            Arrays.fill(d, -1);
+        // ==> TABULATION
+
+        // int rows = grid.length;
+        // int cols = grid[0].length;
+        // int dp[][] = new int[rows][cols];
+
+        // // base case
+        // dp[0][0] = grid[0][0];
+        // int neg = (int) 1e9;
+
+        // for (int i = 0; i < rows; i++) {
+        //     for (int j = 0; j < cols; j++) {
+        //         if (i == 0 && j == 0)
+        //             continue;
+
+        //         int left = j - 1 < 0 ? neg : dp[i][j - 1];
+        //         int up = i - 1 < 0 ? neg : dp[i - 1][j];
+
+        //         dp[i][j] = Math.min(left, up) + grid[i][j];
+        //     }
+        // }
+
+        // return dp[rows - 1][cols - 1];
+
+        // ==> TABULATION
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int prev[] = new int[cols];
+
+        // base case
+        // prev[0] = grid[0][0];
+        int neg = (int) 1e9;
+
+        for (int i = 0; i < rows; i++) {
+            int curr[] = new int[cols];
+            for (int j = 0; j < cols; j++) {
+                if (i == 0 && j == 0) {
+                    curr[j] = grid[i][j];
+                } else {
+                    int left = j - 1 < 0 ? neg : curr[j - 1];
+                    int up = i - 1 < 0 ? neg : prev[j];
+
+                    curr[j] = Math.min(left, up) + grid[i][j];
+                }
+            }
+            prev = curr;
         }
-        return calc(grid, n, m, 0, 0, dp);
+
+        return prev[cols - 1];
     }
 }
