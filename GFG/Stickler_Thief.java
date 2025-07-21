f20e4c9aa31e4ddc2eb4974e761ce1eba861d62d
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-07-22 01:04:46 +0530

    Update Stickler_Thief.java

--- GFG/Stickler_Thief.java
+++ GFG/Stickler_Thief.java
@@ -1,24 +1,60 @@
-class Solution
-{   
-    // Inclusion Exclusion DP Problem
-    private int dp[];
-    private int maxSubsequenceSum(int arr[], int i){
-        if(i<0){
-            // 0 is better than any negative number
+class Solution {
+    public int find(int i, int arr[], int dp[]){
+        if(i < 0){
             return 0;
         }
-        if(dp[i]!=-1){
+        if(dp[i] != -1){
             return dp[i];
         }
-        int exc = maxSubsequenceSum(arr,i-1);
-        int inc = maxSubsequenceSum(arr, i-2) + arr[i]; // minus 2 bcoz cant include consecutive elements
+    
         
-        return dp[i] = Math.max(exc, inc);
+        int loot = arr[i] + find(i - 2, arr, dp);
+    
+        int skip = find(i - 1, arr, dp);
+    
+        return dp[i] = Math.max(loot, skip);
     }
-    public int FindMaxSum(int arr[], int n)
-    {
-        dp = new int[n];
-        Arrays.fill(dp,-1);
-        return maxSubsequenceSum(arr, n-1);
+
+    public int findMaxSum(int arr[]) {
+        // code here
+    //   int n = arr.length;
+    //   int dp[] = new int[n];
+    //   Arrays.fill(dp,-1);
+    //   return find(n-1, arr,dp);
+    
+    // Tabulation
+        // int n = arr.length;
+        // if (n == 0) return 0;
+        // if (n == 1) return arr[0];
+    
+        // int[] dp = new int[n];
+        // dp[0] = arr[0];
+        // dp[1] = Math.max(arr[0], arr[1]);
+    
+        // for (int i = 2; i < n; i++) {
+        //     int loot = arr[i] + dp[i - 2];
+        //     int skip = dp[i - 1];
+        //     dp[i] = Math.max(loot, skip);
+        // }
+    
+        // return dp[n - 1];
+        
+        
+        // SPACE
+        int n = arr.length;
+        if (n == 0) return 0;
+
+        int dpM1 = arr[0];
+        int dpM2 = 0;
+        for (int i = 1; i < n; i++) {
+            int loot = (i-2)>=0 ? arr[i] + dpM2 : arr[i];
+            int skip = dpM1;
+            int ans = Math.max(loot, skip);
+            dpM2 = dpM1;
+            dpM1 = ans;
+            
+        }
+    
+        return dpM1;
     }
 }
