4eede2e354a5a3fed591f0a0214071ae296e52ae
Mit Shah <mitshah2406@gmail.com>
2025-07-22 23:49:14 +0530

    Added 1143-Longest_Common_Subsequence.java

--- Leetcode/1143-Longest_Common_Subsequence.java
+++ Leetcode/1143-Longest_Common_Subsequence.java
@@ -1,47 +1,89 @@
 class Solution {
-    private static int dp[][];
-
-    private int lcs(char[] s1, int i, char[] s2, int j) {
-        if (i < 0 || j < 0) {
+    public int find(int i, int j, String text1, String text2, int dp[][]) {
+        if (i == -1 || j == -1) {
             return 0;
         }
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
         int ans = 0;
-        if (s1[i] == s2[j]) {
-            ans = 1 + lcs(s1, i - 1, s2, j - 1);
-        } else {
-            int x = lcs(s1, i - 1, s2, j);
-            int y = lcs(s1, i, s2, j - 1);
-            ans = Math.max(x, y);
+        if (text1.charAt(i) == text2.charAt(j)) {
+            ans = Math.max(ans, 1 + find(i - 1, j - 1, text1, text2, dp));
         }
+        ans = Math.max(ans, find(i, j - 1, text1, text2, dp));
+        ans = Math.max(ans, find(i - 1, j, text1, text2, dp));
 
         return dp[i][j] = ans;
     }
 
     public int longestCommonSubsequence(String text1, String text2) {
-        char s1[] = text1.toCharArray();
-        char s2[] = text2.toCharArray();
-        int n = s1.length;
-        int m = s2.length;
-        dp = new int[n+1][m+1];
-        // for (int d[] : dp) {
+        // int n = text1.length();
+        // int m = text2.length();
+        // int dp[][] = new int[n][m];
+        // for (int d[] : dp)
         //     Arrays.fill(d, -1);
+        // return find(n - 1, m - 1, text1, text2, dp);
+
+        // int n = text1.length();
+        // int m = text2.length();
+        // int dp[][] = new int[n+1][m+1];
+
+        // // 1. base cases
+        // // already satisfied
+
+        // // 2. changing vars in reverse order
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+        //             dp[i][j] = 1 + dp[i - 1][j - 1];  // characters match
+        //         } else {
+        //             dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);  // no match
+        //         }
+        //     }
         // }
-        // return lcs(s1, n - 1, s2, m - 1);
 
+        // // 4. return same vars as recursive call
+        // // return find(n - 1, m - 1, text1, text2, dp);
+        // return dp[n][m];
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j] = 1+dp[i-1][j-1];
-                }else{
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+        int n = text1.length();
+        int m = text2.length();
+        // int dp[][] = new int[n+1][m+1];
+
+        // rolling arrs
+        int prev[] = new int[m + 1];
+        int curr[] = new int[m + 1];
+        // 1. base cases
+        // already satisfied
+
+        // 2. changing vars in reverse order
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    curr[j] = 1 + prev[j - 1]; // characters match
+                } else {
+                    curr[j] = Math.max(prev[j], curr[j - 1]); // no match
                 }
             }
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        return dp[n][m];
+        // 4. return same vars as recursive call
+        // return find(n - 1, m - 1, text1, text2, dp);
+        return prev[m];
+
     }
 }
+/* 
+Version	                        Time Complexity	    Space Complexity
+----------------------------------------------------------------------
+Recursive (plain)	            Exponential	        O(1)
+Recursive + Memoization 	    O(n * m)	        O(n * m)
+Tabulation (2D array)	        O(n * m)	        O(n * m)
+Space-optimized DP	            O(n * m)	        O(m)
+*/
+// 1,0 - not match
+// 0,1 - not match
+// 1,1 - both match
