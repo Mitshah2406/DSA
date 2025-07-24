cdedae8d8638846c8b20557a0285197faa732bfb
Mit Shah <mitshah2406@gmail.com>
2025-07-24 23:24:12 +0530

    Added 279-Perfect_Squares.java

--- Leetcode/279-Perfect_Squares.java
+++ Leetcode/279-Perfect_Squares.java
@@ -26,7 +26,28 @@ class Solution {
         return dp[i][n] = Math.min(take, skip);
     }
 
+    public int findOpt(int n, int dp[]) {
+        if (n == 1 || n == 0) {
+            return n;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int smallest = (int) 1e9;
+        for (int i = 1; i * i <= n; i++) {
+            int take = findOpt(n - i * i, dp);
+            smallest = Math.min(take, smallest);
+        }
+
+        return dp[n] = smallest + 1;
+    }
+
     public int numSquares(int n) {
+        int dp[] = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return findOpt(n, dp);
+
+       
         // if (n == 1) {
         //     return 1;
         // }
@@ -68,39 +89,39 @@ class Solution {
 
         // return dp[limit][n];
 
-        if (n == 1) {
-            return 1;
-        }
-        int limit = (int) Math.sqrt(n);
-        // int dp[][] = new int[limit + 1][n + 1];
-        int prev[] = new int[n + 1];
-        int curr[] = new int[n + 1];
-        // Arrays.fill(dp[0], (int) 1e9);
-        // dp[0][0] = 0;
-        // 0[0,1e9,1e9]
-        // 1[0,0,0]
-        // 2[0,0,0]
-        Arrays.fill(prev, (int)1e9);
-        prev[0] = 0;
-        
-        for (int i = 1; i <= limit; i++) {
-            for (int j = 1; j <= n; j++) {
-                int take = (int) 1e9;
-                if (i * i <= j) {
-                    int rem = j - (i * i);
-                    if (i * i <= rem) {
-                        take = 1 + curr[rem];
-                    } else {
-                        take = 1 + prev[rem];
-                    }
-                }
-                int skip = prev[j];
+        // if (n == 1) {
+        //     return 1;
+        // }
+        // int limit = (int) Math.sqrt(n);
+        // // int dp[][] = new int[limit + 1][n + 1];
+        // int prev[] = new int[n + 1];
+        // int curr[] = new int[n + 1];
+        // // Arrays.fill(dp[0], (int) 1e9);
+        // // dp[0][0] = 0;
+        // // 0[0,1e9,1e9]
+        // // 1[0,0,0]
+        // // 2[0,0,0]
+        // Arrays.fill(prev, (int)1e9);
+        // prev[0] = 0;
 
-                curr[j] = Math.min(take, skip);
-            }
-            prev = curr.clone();
-        }
+        // for (int i = 1; i <= limit; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         int take = (int) 1e9;
+        //         if (i * i <= j) {
+        //             int rem = j - (i * i);
+        //             if (i * i <= rem) {
+        //                 take = 1 + curr[rem];
+        //             } else {
+        //                 take = 1 + prev[rem];
+        //             }
+        //         }
+        //         int skip = prev[j];
 
-        return prev[n];
+        //         curr[j] = Math.min(take, skip);
+        //     }
+        //     prev = curr.clone();
+        // }
+
+        // return prev[n];
     }
 }
