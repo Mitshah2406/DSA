0c01bf30173b80945233d2f739b0640a8c8379d3
Mit Shah <mitshah2406@gmail.com>
2025-07-25 02:05:03 +0530

    Added 72-Edit_Distance.java

--- Leetcode/72-Edit_Distance.java
+++ Leetcode/72-Edit_Distance.java
@@ -1,79 +1,89 @@
 class Solution {
-    private int dp[][];
-
-    private int solve(char s1[], int i, char[] s2, int j) {
-        if (i == -1 && j == -1) {
-            return 0;
-        }
-        if (i == -1 && j != -1) {
+    public int find(int i, int j, String word1, String word2, int dp[][]) {
+        if (i < 0) {
             return j + 1;
         }
-        if (i != -1 && j == -1) {
+        if (j < 0) {
             return i + 1;
         }
-
         if (dp[i][j] != -1) {
             return dp[i][j];
         }
-
-        if (s1[i] == s2[j]) {
-            dp[i][j] = solve(s1, i - 1, s2, j - 1);
+        int min = (int) 1e9;
+        if (word1.charAt(i) != word2.charAt(j)) {
+            int insert = find(i, j - 1, word1, word2, dp);
+            int delete = find(i - 1, j, word1, word2, dp);
+            int replace = find(i - 1, j - 1, word1, word2, dp);
+            return dp[i][j] = Math.min(Math.min(insert, delete), replace) + 1;
         } else {
-            // add
-            int x = solve(s1, i, s2, j - 1);
-            // replace
-            int y = solve(s1, i - 1, s2, j - 1);
-            // delete
-            int z = solve(s1, i - 1, s2, j);
-
-            dp[i][j] = Math.min(Math.min(x, y), z) + 1;
+            return dp[i][j] = find(i - 1, j - 1, word1, word2, dp);
         }
-
-        return dp[i][j];
     }
 
     public int minDistance(String word1, String word2) {
-        char s1[] = word1.toCharArray();
-        char s2[] = word2.toCharArray();
-        int n = s1.length;
-        int m = s2.length;
-
-        // Memoization
-        // dp = new int[n][m];
-        // for(int d[]: dp){
-        // Arrays.fill(d,-1);
+        // int n = word1.length();
+        // int m = word2.length();
+        // int dp[][] = new int[n][m];
+        // for (int d[] : dp) {
+        //     Arrays.fill(d, -1);
         // }
-        // return solve(s1,n-1,s2,m-1);
+        // return find(n - 1, m - 1, word1, word2, dp);
 
-        // Iterative
-        dp = new int[n + 1][m + 1];
-        // Initialize the base cases
-        // if dono eksaath empty hue to i-1,j-1 hoga that means 0
-        // but if koi ek pehle empty hua toh utne replacements ya deletions ke liye doing this
-        for (int i = 0; i <= n; i++) {
-            dp[i][0] = i; // If s2 is empty, we need i deletions to match
+        // int n = word1.length();
+        // int m = word2.length();
+        // int dp[][] = new int[n + 1][m + 1];
+        // // base cases
+        // for (int i = 0; i <= m; i++) {
+        //     dp[0][i] = i;
+        // }
+        // for (int i = 0; i <= n; i++) {
+        //     dp[i][0] = i;
+        // }
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= m; j++) {
+        //         if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+        //             int insert = dp[i][j - 1];
+        //             int delete = dp[i - 1][j];
+        //             int replace = dp[i - 1][j - 1];
+        //             dp[i][j] = Math.min(Math.min(insert, delete), replace) + 1;
+        //         } else {
+        //             dp[i][j] = dp[i - 1][j - 1];
+        //         }
+        //     }
+        // }
+        // return dp[n][m];
+
+
+        int n = word1.length();
+        int m = word2.length();
+        // int dp[][] = new int[n + 1][m + 1];
+        int curr[] = new int[m+1];
+        int prev[] = new int[m+1];
+        // base cases
+        // for (int i = 0; i <= m; i++) {
+        //     dp[0][i] = i;
+        // }
+        // for (int i = 0; i <= n; i++) {
+        //     dp[i][0] = i;
+        // }
+        for (int i = 0; i <= m; i++) {
+            prev[i] = i;
         }
-        for (int j = 0; j <= m; j++) {
-            dp[0][j] = j; // If s1 is empty, we need j insertions to match
-        }
+
         for (int i = 1; i <= n; i++) {
+            curr[0] = i;
             for (int j = 1; j <= m; j++) {
-                if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
+                if (word1.charAt(i - 1) != word2.charAt(j - 1)) {
+                    int insert = curr[j - 1];
+                    int delete = prev[j];
+                    int replace = prev[j - 1];
+                    curr[j] = Math.min(Math.min(insert, delete), replace) + 1;
                 } else {
-                    // add
-                    int x = dp[i][j - 1];
-                    // replace
-                    int y = dp[i - 1][j - 1];
-                    // del
-                    int z = dp[i - 1][j];
-
-                    dp[i][j] = Math.min(Math.min(x, y), z) + 1;
+                    curr[j] = prev[j - 1];
                 }
             }
+            prev = curr.clone();
         }
-
-        return dp[n][m];
-
+        return prev[m];
     }
 }
