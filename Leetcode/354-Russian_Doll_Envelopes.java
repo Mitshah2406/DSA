c1cd3c7725e309aa3dc07b5e4f184bb51c6d294f
Mit Shah <mitshah2406@gmail.com>
2025-07-27 23:26:09 +0530

    Added 354-Russian_Doll_Envelopes.java

--- Leetcode/354-Russian_Doll_Envelopes.java
+++ Leetcode/354-Russian_Doll_Envelopes.java
@@ -1,42 +1,79 @@
 class Solution {
+    public int find(int i, int prev, int n, int envelopes[][], int dp[][]) {
+        if (i == n) {
+            return 0;
+        }
+        if (dp[i][prev + 1] != -1) {
+            return dp[i][prev + 1];
+        }
+        int take = 0;
+        if (prev == -1 || envelopes[prev][1] < envelopes[i][1]) {
+            take = 1 + find(i + 1, i, n, envelopes, dp);
+        }
+        int skip = find(i + 1, prev, n, envelopes, dp);
+        return dp[i][prev + 1] = Math.max(take, skip);
+    }
+
     public int maxEnvelopes(int[][] envelopes) {
-        // USE LIS {Longest Increasing Subsequence concept }
-
-        // sort any one parameter then apply LIS on the other using BINARY SEARCH ON ANS
         int n = envelopes.length;
-        int dp[] = new int[n];
-        Arrays.sort(envelopes, new Comparator<int[]>() {
-            @Override
-            public int compare(int a[], int b[]) {
-                int ans = a[0] - b[0];
-                if(ans!=0){
-                    return ans;
-                }
-
-                return ans = b[1]-a[1];
+        Arrays.sort(envelopes, (a, b) -> {
+            if (a[0] == b[0]) {
+                return Integer.compare(b[1], a[1]);
             }
+            return Integer.compare(a[0], b[0]);
         });
+        // int dp[][] = new int[n][n + 1];
+        // for (int d[] : dp)
+        //     Arrays.fill(d, -1);
+        // return find(0, -1,n, envelopes, dp);
+        // int dp[] = new int[n];
+        // dp[0] = 1;
+        // int max = 1;
+        // for(int i=1;i<n;i++){
+        //     dp[i]=1;
+        //     for(int j=0;j<i;j++){
+        //         if(envelopes[j][1]<envelopes[i][1]){
+        //             dp[i] = Math.max(dp[i], 1+ dp[j]);
+        //         }
+        //     }
+        //     max = Math.max(max, dp[i]);
+        // }
 
-    
-        int ans = 0;
-        for (int i = 0; i < n; i++) {
-            int low = 0;
-            int high = ans;
-            int curr = envelopes[i][1];
+        // return max;
+
+        int dp[] = new int[n];
+
+        dp[0] = envelopes[0][1];
+        int st = 0;
+        int end = 1;
+        for (int i = 1; i < n; i++) {
+            if (envelopes[i][1] > dp[end - 1]) {
+                dp[end++] = envelopes[i][1];
+                continue;
+            }
+
+            // perform bs for insertion
+            int low = st;
+            int high = end - 1;
+            int ans = end;
             while (low < high) {
                 int mid = low + (high - low) / 2;
-                if (dp[mid] < curr) {
+
+                if (dp[mid] < envelopes[i][1]) {
                     low = mid + 1;
                 } else {
                     high = mid;
                 }
             }
-            dp[low] = curr;
-            if (low == ans) {
-                ans++;
-            }
 
+            dp[high] = envelopes[i][1];
+            if (high == end) {
+                end++;
+            }
         }
-        return ans;
+        return end;
     }
 }
+
+// [[5,4],[6,4],[6,7],[2,3]]
+// [[2,3],[5,4],[6,7],[6,4]]
