0fe365036c872c1825bde676325978d70dc96670
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-07-28 22:57:33 +0530

    Update 1048-Longest_String_Chain.java

--- Leetcode/1048-Longest_String_Chain.java
+++ Leetcode/1048-Longest_String_Chain.java
@@ -1,45 +1,51 @@
 class Solution {
-    private boolean checkIfPossible(String word1, String word2){
-        if(word1.length() != word2.length() + 1) return false;
-        int first = 0, second = 0;
-        while(first < word1.length()){
-            if(second < word2.length() && word1.charAt(first) == word2.charAt(second)){
-                first++; second++;
-            } else first++;
+    public boolean check(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        if (n != m + 1) {
+            return false;
         }
 
-        return (first == word1.length() && word2.length() == second);
+        int first = 0;
+        int second = 0;
+
+        while (first < n) {
+            if (second < m && s1.charAt(first) == s2.charAt(second)) {
+                first++;
+                second++;
+            } else {
+                first++;
+            }
+        }
+
+        return (first == n && second == m);
     }
 
     public int longestStrChain(String[] words) {
+        int n = words.length;
+        // Intution is first we can sort, because we want sequence in any order
+        // then we use lis as longest chaining sequence (lis>(string matching, similar to lcs)
+        // Time - O(nlogn*k) + O(n^2*k)  = O(n^2*k) [k is length of an string]
+        // Space - O(n)
         Arrays.sort(words, (a, b) -> {
             if (a.length() != b.length()) {
-                return a.length() - b.length(); // sort by length
+                return Integer.compare(a.length(), b.length());
             } else {
-                return a.compareTo(b); // lexicographical if lengths are equal
+                return a.compareTo(b);
             }
         });
 
-        int x = words.length;
-        List<Integer> res = new ArrayList<>();
-        int maxi = 1;
-
-        int[] dp = new int[x];
-        Arrays.fill(dp, 1);
-
-        for(int i = 0; i < x; i++){
-            for(int prevInd = 0; prevInd < i; prevInd++){
-                if(checkIfPossible(words[i], words[prevInd]) && 1 + dp[prevInd] > dp[i]){
-                    dp[i] = 1 + dp[prevInd];
+        int dp[] = new int[n];
+        int max = 1;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (check(words[i], words[j]) && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
                 }
-
             }
-            if(dp[i] > maxi){
-                maxi = dp[i];
-            }
+            max = Math.max(dp[i], max);
         }
-
-        // Push all the elements of hash
-        return maxi;
+        return max;
     }
 }
