f109ec2b6897bcf1e241fe339d6c2edaf9df9678
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-07-28 23:52:21 +0530

    Update 300-Longest_Increasing_Subsequence.java

--- Leetcode/300-Longest_Increasing_Subsequence.java
+++ Leetcode/300-Longest_Increasing_Subsequence.java
@@ -1,67 +1,122 @@
 class Solution {
-    private static int index = 0;
-    private static int[] ans = new int[55];
+    public int find(int i, int prev, int n, int nums[], int dp[][]) {
+        if (i == n) {
+            return 0;
+        }
+        if (dp[i][prev + 1] != -1) {
+            return dp[i][prev + 1];
+        }
+        int select = 0;
+        if (prev == -1 || nums[prev] < nums[i]) {
+            select = 1 + find(i + 1, i, n, nums, dp);
+        }
+        int reject = find(i + 1, prev, n, nums, dp);
 
-    static {
-        ans[0] = 4;
-        ans[1] = 4;
-        ans[2] = 1;
-        ans[3] = 1;
-        ans[4] = 1;
-        ans[5] = 2;
-        ans[6] = 3;
-        ans[7] = 2;
-        ans[8] = 2;
-        ans[9] = 1;
-        ans[10] = 1;
-        ans[11] = 1;
-        ans[12] = 3;
-        ans[13] = 3;
-        ans[14] = 1;
-        ans[15] = 6;
-        ans[16] = 3;
-        ans[17] = 5;
-        ans[18] = 6;
-        ans[19] = 6;
-        ans[20] = 6;
-        ans[21] = 7;
-        ans[22] = 2500;
-        ans[23] = 1;
-        ans[24] = 2;
-        ans[25] = 3;
-        ans[26] = 2;
-        ans[27] = 3;
-        ans[28] = 6;
-        ans[29] = 8;
-        ans[30] = 4;
-        ans[31] = 4;
-        ans[32] = 4;
-        ans[33] = 10;
-        ans[34] = 6;
-        ans[35] = 5;
-        ans[36] = 25;
-        ans[37] = 33;
-        ans[38] = 32;
-        ans[39] = 43;
-        ans[40] = 41;
-        ans[41] = 58;
-        ans[42] = 53;
-        ans[43] = 53;
-        ans[44] = 52;
-        ans[45] = 57;
-        ans[46] = 71;
-        ans[47] = 72;
-        ans[48] = 74;
-        ans[49] = 79;
-        ans[50] = 80;
-        ans[51] = 88;
-        ans[52] = 87;
-        ans[53] = 96;
-        ans[54] = 1;
+        return dp[i][prev + 1] = Math.max(select, reject);
     }
-    
+
     public int lengthOfLIS(int[] nums) {
-        return ans[index++];
+        // int n = nums.length;
+        // int dp[][] = new int[n][n + 1];
+        // for (int d[] : dp)
+        //     Arrays.fill(d, -1);
+        // return find(0, -1, n, nums, dp);
+
+        // // tabulation
+
+        // int n = nums.length;
+        // int dp[][] = new int[n + 1][n + 1];
+
+        // // return find(0, -1, n, nums, dp);
+
+        // for (int i = n - 1; i >= 0; i--) {
+        //     for (int prev = i - 1; prev >= -1; prev--) {
+        //         int select = 0;
+        //         if (prev == -1 || nums[prev] < nums[i]) {
+        //             select = 1 + dp[i + 1][i + 1];
+        //         }
+        //         int reject = dp[i + 1][prev + 1];
+        //         dp[i][prev + 1] = Math.max(select, reject);
+        //     }
+        // }
+
+        // return dp[0][0];
+
+        // tabulation - space
+
+        // int n = nums.length;
+        // // int dp[][] = new int[n + 1][n + 1];
+        // int ahead[] = new int[n+1];
+        // int curr[] = new int[n+1];
+        // // return find(0, -1, n, nums, dp);
+
+        // for (int i = n - 1; i >= 0; i--) {
+        //     for (int prev = i - 1; prev >= -1; prev--) {
+        //         int select = 0;
+        //         if (prev == -1 || nums[prev] < nums[i]) {
+        //             select = 1 + ahead[i + 1];
+        //         }
+        //         int reject = ahead[prev + 1];
+        //         curr[prev + 1] = Math.max(select, reject);
+        //     }
+        //     ahead = curr;
+        // }
+
+        // return ahead[0];
+
+        // O(n) space approach
+        // int n = nums.length;
+        // int dp[] = new int[n];
+        // Arrays.fill(dp, 1);
+        // int ans = 1;
+        // for (int i = 1; i < n; i++) {
+        //     int res = 1;
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j]<nums[i]){
+        //             res = Math.max(1+dp[j], res);
+        //         }
+        //     }
+        //     dp[i] = res;
+        //     ans = Math.max(ans, dp[i]);
+        // }  
+        // return ans; 
+
+
+        // Binary Search Approach
+        int n = nums.length;
+        int dp[] = new int[n];
+        int start = 0;
+        int end = 1;
+        dp[0] = nums[0];
+        for(int i=1;i<n;i++){
+            if(nums[i]>dp[end-1]){
+                dp[end++] = nums[i];
+                continue;
+            }
+
+            // perform bs
+
+            int low = start;
+            int high = end-1;
+            int ans = -1;
+            while(low<=high){
+                int mid = low + (high-low)/2;
+                if(dp[mid]==nums[i]){
+                    ans = mid;
+                    break;
+                }else if(dp[mid]>nums[i]){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+            }
+            if(ans==-1){
+                ans = low;
+            }
+
+            dp[ans] = nums[i];
+        }
+
+        return end;
     }
-    
 }
