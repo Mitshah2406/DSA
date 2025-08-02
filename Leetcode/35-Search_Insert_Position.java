d70707ad324d5388544e5e381f95e4668b5f51da
Mit Shah <mitshah2406@gmail.com>
2025-08-02 16:48:48 +0530

    Added 35-Search_Insert_Position.java

--- Leetcode/35-Search_Insert_Position.java
+++ Leetcode/35-Search_Insert_Position.java
@@ -1,22 +1,37 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int n = nums.length;
+    /*
+        Time - O(logn)
+     */
+    public int searchInsert(int[] arr, int target) {
+    int n = arr.length;
+    int low = 0;          // Start pointer at the beginning of the array
+    int high = n - 1;     // End pointer at the last index
 
-        int left = 0;
-        int right = n-1;
+    // Binary search loop: runs while search space is valid
+    while (low <= high) {
+        int mid = low + (high - low) / 2;  // Find middle index safely
 
-        while(left<=right){
-            int mid = left + (right-left)/2;
-
-            if(nums[mid]==target){
-                return mid; // if found give mid, else left
-            }else if(nums[mid]>target){
-                right = mid-1;
-            }else{
-                left = mid+1;
-            }
+        // If mid element is greater than or equal to target:
+        // - We might have found the target OR
+        // - The target should be inserted before this mid
+        // So, we shrink the search space to the LEFT half.
+        if (arr[mid] >= target) {
+            high = mid - 1;
+        } 
+        // If mid element is less than target:
+        // - The target must be on the RIGHT side.
+        else {
+            low = mid + 1;
         }
-
-        return left;
     }
+
+    // At this point, loop ends when low > high.
+    // - 'high' points to the last index < target
+    // - 'low' points to the first index >= target
+    // Hence, 'low' is the correct insertion position.
+    // If target exists, low will be its index.
+    // If target doesn't exist, low is the place to insert it.
+    return low;
+}
+
 }
