b65ba132736f624d5b15abb8c050ff87df1b828c
Mit Shah <mitshah2406@gmail.com>
2025-08-11 18:04:36 +0530

    Added 2220-Minimum_Bit_Flips_to_Convert_Number.java

--- Leetcode/2220-Minimum_Bit_Flips_to_Convert_Number.java
+++ Leetcode/2220-Minimum_Bit_Flips_to_Convert_Number.java
@@ -1,27 +1,3 @@
-class Solution {
-    private boolean checkBit(int n, int k) {
-        if ((n & (1 << k)) == 0) {
-            return false; // unset
-        }
-        return true; // set
-    }
-
-    public int minBitFlips(int start, int goal) {
-        if (start == goal)
-            return 0;
-        int ans = 0;
-        int place = 0;
-        for (int i = 0; i < 32; i++) {
-            place = place + (1 << i); // binary number logic ==> ...64 32 16 8 4 2 1
-            if ((checkBit(start, i) ^ checkBit(goal, i))) {
-                // if bits are diff then xor will result in true, which we want
-                ans++;
-            }
-            if (start <= place && goal <= place) {
-                break;
-            }
-        }
-
-        return ans;
-    }
-}
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        return (start ^ goal).bit_count()
