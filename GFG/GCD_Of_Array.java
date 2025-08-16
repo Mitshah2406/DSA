d3dd41bd4f7a6186d421cf0cb63fd3f26c4f4075
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-08-16 23:39:12 +0530

    Update GCD_Of_Array.java

--- GFG/GCD_Of_Array.java
+++ GFG/GCD_Of_Array.java
@@ -1,22 +1,44 @@
-class Solution {
-    public int findGcd(int dividend, int divisor){
-        if(divisor==0){
-            return dividend;
-        }
-        int res = findGcd(divisor, dividend%divisor);
-        return res;
-    }
-    public int gcd(int n, int arr[]) {
+// User function Template for Java
 
+class Solution {
+    /*
+    Time = O(nlogn)
+    */
+    public int gcd(int n, int arr[]) {
         if(n==1){
             return arr[0];
         }
-        int res = findGcd(arr[0], arr[1]);
         
+        int res = arr[0];
         for(int i=1;i<n;i++){
-            res = findGcd(res, arr[i]);
+            res = findGCDIterative(res,arr[i]);
         }
         
         return res;
+    }
+    /*
+    findGCD() has time of O(log phi min(a,b))
+    */
+    private int findGCDRecursive(int a, int b){
+        if(a==0){
+            return b;
+        }
+        
+        if(a>b){
+            return findGCDRecursive(a%b,b);
+        }else{
+            return findGCDRecursive(b%a,a);
+        }
+    }
+    // O(log phi min(a,b))
+    private int findGCDIterative(int a, int b){
+        while(a>0 && b>0){
+            if(a>b){
+                a = a%b;
+            }else{
+                b = b%a; 
+            }
+        }
+        return a==0 ? b : a;
     }
 }
