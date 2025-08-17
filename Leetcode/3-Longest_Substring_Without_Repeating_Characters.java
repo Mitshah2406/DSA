930ceb79ea420e5ea70908ae55a40c09389c0c0e
Mit Shah <mitshah2406@gmail.com>
2025-08-17 23:53:25 +0530

    Added 3-Longest_Substring_Without_Repeating_Characters.java

--- Leetcode/3-Longest_Substring_Without_Repeating_Characters.java
+++ Leetcode/3-Longest_Substring_Without_Repeating_Characters.java
@@ -1,23 +1,83 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int l = 0;
-        int r = 0;
-        int maxLen = 0;
-        int arr[] = new int[256];
-        Arrays.fill(arr,-1);
-        int n = s.length();
-        while(r<n){
-            if(arr[s.charAt(r)]!=-1){
-                if(arr[s.charAt(r)]>=l){
-                    l = arr[s.charAt(r)] + 1;
-                }
-            }
-            int len = r-l+1;
-            maxLen = Math.max(maxLen, len);
-            arr[s.charAt(r)]=r;
-            r++;
-        }
+        /**
+        Brute Force
+        O(n^3) , O(1)
+         */
+        // int n = s.length();
+        // int max = 0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=n-1;j>=i;j--){
+        //         int map[] = new int[256];
+        //         boolean flag = true;
+        //         for(int k=i;k<=j;k++){
+        //             if(map[s.charAt(k)]<1){
+        //                 map[s.charAt(k)]++;
+        //             }else{
+        //                 flag = false;
+        //                 break;
+        //             }
+        //         }
 
-        return maxLen;
+        //         if(flag) max = Math.max(max, j-i+1);
+        //     }
+        // }
+
+        // return max;
+
+        /**
+        BETTER
+        Intuition - Keep a CharacterXFrequency Map
+        and keep a left and right ptr, and keep pruning the left pointer
+
+        O(n), O(N)
+         */
+
+        // int n = s.length();
+        // int max = 0;
+
+        // int left = 0;
+        // HashMap<Character, Integer> hm = new HashMap<>();
+
+        // for(int right=0;right<n;right++){
+        //     char curr = s.charAt(right);
+        //     hm.put(curr, hm.getOrDefault(curr,0)+1);
+        //     // prune the window
+        //     while(hm.get(curr)>1){
+        //         char rem = s.charAt(left);
+        //         int val = hm.get(rem);
+        //         hm.put(rem, val-1);
+        //         left++;
+        //     }
+        //     max = Math.max(right-left+1, max);
+        // }
+        // return max;
+
+        /**
+        OPTIMAL
+        O(N), O(N)
+
+        Change the map to CharacterXLastSeenIdx
+        So that inner loop eliminates, we directly jump to lastSeen+1 idx for left pruning
+
+        condition is lastSeen>=left coz when jumping some other characters can also be pruned which are in the map
+         */
+        int n = s.length();
+        int max = 0;
+
+        int left = 0;
+        HashMap<Character, Integer> hm = new HashMap<>(); // Char X last seen idx
+
+        for(int right=0;right<n;right++){
+            char curr = s.charAt(right);
+            int getLastSeenIdx = hm.getOrDefault(curr,-1);
+            // prune the window
+            if(getLastSeenIdx>=left){
+                left = getLastSeenIdx+1;
+            }
+            hm.put(curr,right);
+            max = Math.max(right-left+1, max);
+        }
+        return max;
     }
 }
