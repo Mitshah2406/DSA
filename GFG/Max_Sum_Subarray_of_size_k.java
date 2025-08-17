54016948230077baa6f0ca500c6c7e3e443cb7cb
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-08-17 18:59:00 +0530

    Update Max_Sum_Subarray_of_size_k.java

--- GFG/Max_Sum_Subarray_of_size_k.java
+++ GFG/Max_Sum_Subarray_of_size_k.java
@@ -1,20 +1,27 @@
 class Solution {
     public int maximumSumSubarray(int[] arr, int k) {
         // Code here
+        // Traditional Fixed Size Sliding Window
+        // O(n)
+        // O(1)
         int n = arr.length;
         int sum = 0;
-        int max = 0;
+        int ans = 0;
+        // calculating for first window
         for(int i=0;i<k;i++){
             sum+=arr[i];
         }
-        max = Math.max(max, sum);
-        
+        ans = sum;
+        // next windows
         for(int i=k;i<n;i++){
+            // remove sum of leaving element of last window
             sum-=arr[i-k];
+            // add sum of new element in window
             sum+=arr[i];
-            max = Math.max(max, sum);
+            ans = Math.max(ans,sum);
         }
         
-        return max;
+        return ans;
+        
     }
 }
