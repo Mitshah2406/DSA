c685d01387801f22d419ca5edfeb126c172dfb16
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-08-18 18:55:41 +0530

    Update Longest_Substring_with_K_Uniques.java

--- GFG/Longest_Substring_with_K_Uniques.java
+++ GFG/Longest_Substring_with_K_Uniques.java
@@ -1,33 +1,89 @@
 class Solution {
     public int longestKSubstr(String s, int k) {
-        // code here
-        int n = s.length();
-        if(k>n) return 0;
+        /*
+        BETTER Solution
+        O(2N), O(1)
+        Dynamic size sliding window, with a hashmap to store frequencies
+        */
+        // HashMap<Character, Integer> hm = new HashMap<>();
+        
+        // int n = s.length();
+        
+        // int left = 0;
+        // int right = 0;
+        // int ans = -1;
+        // while(right<n){
+        //     char curr = s.charAt(right);
+        //     hm.put(curr, hm.getOrDefault(curr,0)+1);
+            
+        //     while(hm.size()>k){
+        //         // remove left
+        //         char l = s.charAt(left);
+        //         int val = hm.get(l);
+        //         val--;
+        //         if(val>0){
+        //             hm.put(l,val);
+        //         }else{
+        //             hm.remove(l);
+        //         }
+                
+        //         left++;
+        //     }
+        //     if(hm.size()==k){
+        //         ans = Math.max(ans, right-left+1);
+        //     }
+        //     right++;
+            
+        // }
+        // return ans;
+        
+        
+        /*
+        OPTIMAL Solution
+        O(N), O(1)
+        Dynamic size sliding window, with a hashmap to store frequencies
+        Just removed the inner loop, replaced with if statement,
+        and added a check for updating the ans, as ans will never decrease 
+        from the length it has
+        So no point in pruning whole left, that anyways gonna not become whole ans
+        
+        aaabbccd, k=2
+         l   r
+        till c max len recorded was 5, once we get c, we move l one time forward
+        a's are invalid but that doesnt anyways affect our answer, we are just 
+        preventing the window to be pruned from less than 5
+        
+        */
         HashMap<Character, Integer> hm = new HashMap<>();
+        
+        int n = s.length();
+        
         int left = 0;
+        int right = 0;
         int ans = -1;
-        for(int right=0;right<n;right++){
+        while(right<n){
             char curr = s.charAt(right);
             hm.put(curr, hm.getOrDefault(curr,0)+1);
             
-            if(left<right && hm.size()>k){
-                while(hm.size()!=k){
-                    char temp = s.charAt(left);
-                    int val = hm.get(temp);
-                    if(val-1 == 0){
-                        hm.remove(temp);
-                    }else{
-                        hm.put(temp, val-1);
-                    }
-                    left++;
+            if(hm.size()>k){
+                // remove left
+                char l = s.charAt(left);
+                int val = hm.get(l);
+                val--;
+                if(val>0){
+                    hm.put(l,val);
+                }else{
+                    hm.remove(l);
                 }
+                
+                left++;
             }
-            
             if(hm.size()==k){
-                ans = Math.max(right-left+1,ans);
-            } 
+                ans = Math.max(ans, right-left+1);
+            }
+            right++;
+            
         }
-        
         return ans;
     }
 }
