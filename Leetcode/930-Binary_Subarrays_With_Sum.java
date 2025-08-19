c0e2cb237be679dfc7765c1af1496a2a71ec9aba
Mit Shah <mitshah2406@gmail.com>
2025-08-19 20:25:46 +0530

    Added 930-Binary_Subarrays_With_Sum.java

--- Leetcode/930-Binary_Subarrays_With_Sum.java
+++ Leetcode/930-Binary_Subarrays_With_Sum.java
@@ -1,27 +1,15 @@
-class Solution {
-    // two pointer technique to find subarrays with sum <= goal
-    public int temp(int[] nums, int goal) {
-        if(goal<0) return 0;
-        int l= 0;
-        int r = 0;
-        int sum = 0;
-        int cut = 0;
-        int n = nums.length;
-        while(r<n){
-            sum+=nums[r];
-
-            while(sum>goal){
-                sum = sum - nums[l];
-                l++;
-            }
-            cut = cut + (r-l+1); // right to left inclusive subarray consider
-            r = r+1;
-        }
-
-        return cut;
-    }
-    public int numSubarraysWithSum(int nums[], int goal){
-        // find <=goal - <=goal-1 = =goal
-        return temp(nums,goal) - temp(nums,goal-1);
-    }
-}
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        count = 0
+        prefix_sum = 0
+        sum_count = defaultdict(int)
+        
+        for num in nums:
+            prefix_sum += num
+            if prefix_sum == goal:
+                count += 1
+            if prefix_sum - goal in sum_count:
+                count += sum_count[prefix_sum - goal]
+            sum_count[prefix_sum] += 1
+        
+        return count
