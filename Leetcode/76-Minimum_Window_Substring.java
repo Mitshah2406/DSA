e8156b908198508147f3df0c90f9e82616a08e4e
Mit Shah <mitshah2406@gmail.com>
2025-08-20 18:28:46 +0530

    Added 76-Minimum_Window_Substring.java

--- Leetcode/76-Minimum_Window_Substring.java
+++ Leetcode/76-Minimum_Window_Substring.java
@@ -1,63 +1,62 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int tLen = t.length();
-        int sLen = s.length();
-        if(tLen>sLen){
-            return "";
+        /**
+        BRUTE FORCE - Time - O(n^2), Space - O(256)
+        Simple nested loop soln
+         */
+
+
+        /**
+        OPTIMAL
+        Time = O(2N), Space = O(256)
+        Take a  map[256] 
+        Take inital frequencies by incrementing in map,
+        then in the window
+        inserting means DECREMENT, removing means INCREMENT
+        first, just insert all chars of t,
+        then run a sliding window, in that add each character, if it was >0 means a valid character is being added to window (currCnt++)
+
+        if we have a valid window (currCnt==m), then just try pruning from left, basically incrementing in map,
+        if someone goes above 0, then we pruned a valid char, so decrement currCnt ctr
+
+        keep checking and updating minLen and startIdx in the process,
+        finally if startIdx never updated return "", else return substring from startIdx to startIdx+minLen
+         */
+        int n = s.length();
+        int m = t.length();
+        int minLen = (int)1e9;
+        int startIdx = -1;
+        int map[] = new int[256];
+        for (int i=0;i<m;i++) {
+            map[t.charAt(i)]++; // to ensure that only valid chars are > 0
         }
-        char[] sArr = s.toCharArray();
-        char[] tArr = t.toCharArray();
-        HashMap<Character, Integer> tMap = new HashMap();
-        HashMap<Character, Integer> sMap = new HashMap();
-
-        for (int i=0;i<tLen;i++) {
-            tMap.put(tArr[i], tMap.getOrDefault(tArr[i], 0) + 1);
-        }
-
-        int sp = 0;
-        int ep = 0;
-        int ans = Integer.MAX_VALUE;
-        int matchCount = 0;
-        int subStart = 0;
-        int subEnd = 0;
-        while(ep<sLen){
-            if(matchCount==tLen){
-                if(ep-sp<ans){
-                    ans = ep-sp;
-                    subStart = sp;
-                    subEnd = ep;
-                }
-                sMap.put(sArr[sp], sMap.get(sArr[sp])-1);
-
-                if(sMap.get(sArr[sp])<tMap.getOrDefault(sArr[sp],0)){
-                    matchCount--;
-                }
-                sp++;
-            }else{
-                sMap.put(sArr[ep], sMap.getOrDefault(sArr[ep], 0)+1);
-
-                if(sMap.get(sArr[ep])<=tMap.getOrDefault(sArr[ep], 0)){
-                    matchCount++;
-                }
-
-                ep++;
+        int left = 0;
+        int right = 0;
+        int currCnt = 0;
+        while (right < n) {
+            char curr = s.charAt(right);
+            if(map[curr]>0){ // if it was a valid character, increase currCnt
+                currCnt++;
             }
-        }
-        while(matchCount==tLen){
-            if(matchCount==tLen){
-                if(ep-sp<ans){
-                    ans = ep-sp;
-                    subStart = sp;
-                    subEnd = ep;
-                }
-                sMap.put(sArr[sp], sMap.get(sArr[sp])-1);
+            map[curr]--;
 
-                if(sMap.get(sArr[sp])<tMap.getOrDefault(sArr[sp],0)){
-                    matchCount--;
+            while(currCnt==m){
+                char toBeChecked = s.charAt(left);
+                if((right-left+1)<minLen){
+                    minLen = (right-left+1);
+                    startIdx = left;
                 }
-                sp++;
+                map[toBeChecked]++;
+                if(map[toBeChecked]>0){
+                    // means increase currCnt, because we removed a valid character
+                    currCnt--;
+                }
+                left++;
             }
+
+            right++;
         }
-        return s.substring(subStart, subEnd);
+
+        return startIdx==-1 ? "" : s.substring(startIdx, startIdx+minLen);
     }
 }
