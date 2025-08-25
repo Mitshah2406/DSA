6b48408a3ac78d65cfba8232e24868e0c7ba0b56
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-08-25 12:32:48 +0530

    Refactor subarray counting methods for clarity

--- GFG/Subarrays_with_equal_1s_and_0s.java
+++ GFG/Subarrays_with_equal_1s_and_0s.java
@@ -1,32 +1,44 @@
-//User function Template for Java
-
-class Solution
-{
-    //Function to count subarrays with 1s and 0s.
-    static int countSubarrWithEqualZeroAndOne(int arr[], int n)
-    {
-            
-            HashMap<Integer, Integer> hm = new HashMap();
-            int p0 = 0; // prefix count of 0
-            int p1=0; // prefix count of 1
-            // you could also take an array of prefixes instead
-            int ans = 0;
-            hm.put(0,1);
-            for(int i=0;i<n;i++){
-                if(arr[i]==0){
-                    p0++;
-                }else{
-                    p1++;
-                }
-                int diff = p0-p1;
-                
-               ans+= hm.getOrDefault(diff, 0);
-               
-               hm.put(diff,hm.getOrDefault(diff, 0)+1);
+class Solution {
+    private int brute(int arr[], int n){
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            int zeroes = 0;
+            int ones = 0;
+            for(int j=i;j<n;j++){
+                if(arr[j]==1) ones++;
+                else zeroes++;
+                if(ones==zeroes) ans++;
+            }
+        }
+        return ans;
+    }
+    private int optimal(int arr[], int n){
+        // 1 - means 1
+        // 0 - means -1
+        // Same as count subarrays equals to k, (here it is 0)
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        
+        hm.put(0,1);
+        int sum = 0, cnt = 0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==1){
+                sum+=1;
+            }else{
+                sum-=1;
             }
             
-            return ans; 
+            cnt+=hm.getOrDefault(sum,0);
+            hm.put(sum,hm.getOrDefault(sum,0)+1);
+            
+        }
+        
+        return cnt;
+    }
+    public int countSubarray(int[] arr) {
+        // code here
+        int n = arr.length;
+        
+        // return brute(arr,n);
+        return optimal(arr,n);
     }
 }
-
-
