eabb446956a1648d1fea48b3ef5a9ca39841e964
Mit Shah <84562856+Mitshah2406@users.noreply.github.com>
2025-10-02 22:34:41 +0530

    Update Merge_Sort.java

--- GFG/Merge_Sort.java
+++ GFG/Merge_Sort.java
@@ -1,82 +1,48 @@
-// Merge Sort
-// Difficulty: Medium
-// Given an array arr[], its starting position l and its ending position r. Sort the array using merge sort algorithm.
-// Example 1:
-
-// Input:
-// N = 5
-// arr[] = {4 1 3 9 7}
-// Output:
-// 1 3 4 7 9
-// Example 2:
-
-// Input:
-// N = 10
-// arr[] = {10 9 8 7 6 5 4 3 2 1}
-// Output:
-// 1 2 3 4 5 6 7 8 9 10
-
-// Your Task:
-// You don't need to take the input or print anything. Your task is to complete the function merge() which takes arr[], l, m, r as its input parameters and modifies arr[] in-place such that it is sorted from position l to position r, and function mergeSort() which uses merge() to sort the array in ascending order using merge sort algorithm.
-
-// Expected Time Complexity: O(nlogn) 
-// Expected Auxiliary Space: O(n)
-
-// Constraints:
-// 1 <= N <= 105
-// 1 <= arr[i] <= 105
-
-//Link --> https://www.geeksforgeeks.org/problems/merge-sort/1?utm_source=gfg
-
-class Solution
-{
-    void merge(int arr[], int l, int m, int r)
-    {
-         int b[] = new int[r-l+1];
-         int i=l;
-         int j = m+1;
-         int k = 0;
-         
-         while(i<=m && j<=r){
-             if(arr[i]<=arr[j]){
-                 b[k] = arr[i];
-                 i++;
-             }else{
-                  b[k] = arr[j];
-                j++;
-             }
-             k++;
-         }
-
-            while(i<=m){
-             b[k] = arr[i];
-             i++;
-             k++;
+class Solution {
+    private void merge(int arr[], int st, int mid, int end){
+        int res[] = new int[end-st+1];
+        int i = st, j = mid+1, k = 0;
+        
+        while(i<=mid && j<=end){
+            if(arr[i]<=arr[j]){
+                res[k++] = arr[i++];
+            }else{
+                res[k++] = arr[j++];
             }
-   
-            while(j<=r){
-             b[k] = arr[j];
-             j++;
-             k++;
-            }
-
-         
-         for(int f=0;f<b.length;f++){
-             arr[l+f] = b[f];
-         }
-         
+        }
+        
+        while(i<=mid){
+            res[k++] = arr[i++];
+        }
+        
+        while(j<=end){
+            res[k++] = arr[j++];
+        }
+        
+        for(int e=0;e<res.length;e++){
+            arr[e+st] = res[e];
+        }
+        
     }
-    void mergeSort(int arr[], int l, int r)
-    {
-        if(l==r) return;
-            int mid = (l+r)/2;
-            mergeSort(arr, l,mid);
-            mergeSort(arr,mid+1,r);
+    private void mSort(int arr[], int l, int r){
+        
+        if(l<r){
+            int mid = l + (r-l)/2;
+            mSort(arr, l, mid);
+            mSort(arr,mid+1, r);
             
-            merge(arr,l,mid,r);
+            merge(arr, l , mid, r);
+        }
+    }
+    void mergeSort(int arr[], int l, int r) {
+        // code here
+        int n = arr.length;
+        
+        mSort(arr, l ,r);
     }
 }
 
-
-// TC - O(nlogn)
-// SC - O(n)
+/**
+ * Time - O(nlogn)
+ * Space - O(n) 
+ * /
