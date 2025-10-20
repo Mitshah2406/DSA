91b07849e404e884c159ebd3a66a97234bdca56e
Mit Shah <mitshah2406@gmail.com>
2025-10-20 17:33:43 +0530

    Added 429-N-ary_Tree_Level_Order_Traversal.java

--- Leetcode/429-N-ary_Tree_Level_Order_Traversal.java
+++ Leetcode/429-N-ary_Tree_Level_Order_Traversal.java
@@ -18,31 +18,32 @@ class Node {
 */
 
 class Solution {
+    // private void find(Node root, List<List<Integer>> res, List<Integer> curr){
+    //     if(root==null) return;
+    //     curr.add(root.val);
+    //     for(Node n : root.children){
+    //         find(n, res, curr);
+    //     }
+    // }
     public List<List<Integer>> levelOrder(Node root) {
-        List<List<Integer>> res = new ArrayList();
+        List<List<Integer>> res = new ArrayList<>();
         if(root==null){
             return res;
         }
-        Queue<Node> q = new LinkedList();
+        Queue<Node> q = new LinkedList<>();
         q.add(root);
-
         while(q.size()!=0){
             int n = q.size();
-            List<Integer> arr = new ArrayList();
+            ArrayList<Integer> arr = new ArrayList<>();
             for(int i=0;i<n;i++){
                 Node rem = q.poll();
                 arr.add(rem.val);
-
-                int size = rem.children.size();
-
-                for(int j=0;j<size;j++){
-                    Node child = rem.children.get(j);
-                    q.add(child);
+                for(Node no: rem.children){
+                    q.add(no);
                 }
             }
             res.add(arr);
         }
-
         return res;
     }
 }
